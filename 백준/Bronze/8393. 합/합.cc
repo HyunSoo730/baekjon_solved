1661cc3546d617ea7a9d4c55d837452156bd869a
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2023-02-02 21:36:02 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int n, i, sum = 0;
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		sum = sum + i;
+	}
+	printf("%d", sum);
+	return 0;
+}
