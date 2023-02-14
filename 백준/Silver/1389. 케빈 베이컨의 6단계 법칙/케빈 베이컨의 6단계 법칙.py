85bed55710958afda73363753892650772f4faa7
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2023-02-14 17:33:45 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 64 ms, Memory: 34136 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -1,4 +1,5 @@
 import sys
+
 input = sys.stdin.readline
 from collections import deque
 
@@ -16,23 +17,22 @@ for _ in range(m):
 def BFS(v):
     dq = deque()
     ch[v] = 1 #방문 처리
-    dq.append((v,1)) #노드, 거리
-    dis = 0
+    dq.append(v) #노드, 거리
     while dq:
-        now, length = dq.popleft() #현재 노드
+        now = dq.popleft() #현재 노드
         for x in g[now]: #현재 노드의 인접 노드
             if ch[x] == 0: #아직 방문 전
                 ch[x] = 1 #
-                dis += length
-                dq.append((x,length+1))
+                dis[x] = dis[now] + 1 #x까지 가는 최단거리는 now까지의 거리 + 1
+                dq.append(x)
                 # print("length : " , length)
         # print(dis)
-    return dis
 res = []
 for i in range(1,n+1):
     ch = [0] * (n+1)
-    dis = BFS(i)
-    res.append(dis)
+    dis = [0] * (n+1)   #체크 변수와 dis 최단거리 리스트를 따로 만들어도 될까..? 하나로 통합해서 사용하는거 연습해야 할듯?
+    BFS(i)
+    res.append(sum(dis))
 
 r_idx = res.index(min(res))
 print(r_idx + 1)
