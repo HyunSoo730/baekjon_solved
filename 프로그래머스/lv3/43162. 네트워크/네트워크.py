e1f9511795592aaba86f5c4940b089a99fe6d4ea
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2023-02-24 00:04:06 +0900

    [level 3] Title: 네트워크, Time: 0.20 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,28 +1,16 @@
-def DFS(v):
-    global g
-    global ch
-    global N
-    ch[v] = 1
-    for i in range(N):
-        if g[v][i] == 1 and ch[i] == 0:
-            DFS(i)
-
-
 def solution(n, computers):
-    #n개의 노드, 연결상태 computers
-    global g
-    global ch
-    global N
-    N = n
-    ch = [0] * n
+    #n은 노드 개수 computers는 2차원 그래프
     g = computers
-    cnt = 0
     ch = [0] * n
+    
+    def DFS(v):
+        ch[v] = 1
+        for i in range(n):
+            if g[v][i] == 1 and ch[i] == 0:
+                DFS(i)
+    cnt = 0
     for i in range(n):
         if ch[i] == 0:
             DFS(i)
             cnt += 1
     return cnt
-        
-    
-        
