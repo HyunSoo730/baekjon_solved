fed54e78249bee57e372fdaca611af4c266f5db1
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2023-02-27 19:13:18 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,14 +1,16 @@
 import sys
 
-
 T = int(input())
-#n을 1,2,3의 합으로 나타내는 방법의 수
+dp = [0] * 100
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4 #자명한 것.
 for _ in range(T):
     n = int(input())
-    dp = [0] * (100) #dp[i]는 i값을 만드는데 필요한 1,2,3 방법 개수
-    dp[1] = 1 
-    dp[2] = 2
-    dp[3] = 4   #자명한 것들 미리 초기화
-    for i in range(4,n+1): #마지막 항이 1,2,3으로 끝날때는 생각?
-        dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+    if dp[n] > 0: #이미 있음
+        print(dp[n])
+        continue
+    for i in range(4,n+1):
+        dp[i] = dp[i-3] + dp[i-2] + dp[i-1]
     print(dp[n])
+    
