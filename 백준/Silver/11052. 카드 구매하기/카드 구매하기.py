c09bcfc99d4da050627246c6fb52012b797f257a
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2023-02-27 19:44:02 +0900

    [Silver I] Title: 카드 구매하기, Time: 244 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -1,25 +1,21 @@
 import sys
 
 
-#8가지 색 존재
-#카드 1~n개 포함된 카드팩. 카드팩 n가지 존재
-#가격이 비싸면 높은 등급의 카드
-#비싼걸로 사기 가치?? 조건 2개
-#가방 문제랑 동일? 냅색 알고리즘
 
 n = int(input())
-data = list(map(int, input().split())) 
-data.insert(0,0) #1번 인덱스부터 활용하기 위해
-#data[i]는 i개의 카드를 사는데 data[i]값이 든다는 의미
-dp = [0] * 1001
-dp[1] = data[1] #자명한 사실.
-#dp[i]는 i개의 카드를 사는데 드는 최댓값
+data = list(map(int, input().split()))
+
+#n개를 갖기 위해 지불해야 하는 금액의 최댓값
+data.insert(0,-1)
+dp = [0] * (n+1)
+#dp[i]는 i개의 카드를 살 때 지불하는 최댓값.
+dp[1] = data[1] #1개를 뽑는데는 자명하게 1개 카드팩
 for i in range(1,n+1):
-    cnt = i   #개수
-    val = data[i]  #가격
+    cnt = i
+    val = data[i]
     for j in range(cnt, n+1):
-        #현재 카드팩을 산다고 생각했을 때
-        caseA = dp[j-cnt] + val
-        caseB = dp[j]
+        caseA = dp[j] #기존에 j개의 카드를 살 때의 최댓값
+        caseB = dp[j-cnt] + val #cnt개의 카드팩을 살떄 가격
         dp[j] = max(caseA, caseB)
+
 print(dp[n])
