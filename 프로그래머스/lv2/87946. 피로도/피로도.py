cd42d0cdf9432bc7acd8e783ec46d172926cdf5b
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2023-03-04 01:48:00 +0900

    [level 2] Title: 피로도, Time: 0.10 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.py
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -1,22 +1,18 @@
 import itertools
 def solution(k, dungeons):
     #피로도 k
-    #각 던전별 최소 필요도, 소모 피로도 2차원 배열로 주어짐
     data = dungeons
-    #유저가 탐험할 수 있는 최대 던전 수 리턴.
-    n = len(data)
-    res = 0
-    temp = list(range(n))
+    n = len(data) #던전 수
     
-    for e in itertools.permutations(temp, n):
-        #e에 저장된 인덱스 순서로 탐험할 때...
-        for i in range(n):
-            p = k
-            cnt = 0
-            for idx in e:
-                a,b = data[idx][0], data[idx][1]
-                if p >= a:
-                    p -= b
-                    cnt += 1
-            res = max(res, cnt)
+    res = 0
+    for temp in itertools.permutations(data, n):
+        #탐험 순서 temp에 저장
+        p = k
+        cnt = 0
+        for a,b in temp:
+            #a,b는 각각 최소 피로도, 소모 피로도
+            if p >= a:
+                p -= b
+                cnt += 1
+        res = max(res, cnt)
     return res
