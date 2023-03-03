324b500e662a91ee0db0b757fe7860b03725956b
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2023-03-04 01:50:40 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.10 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,49 +1,72 @@
-import itertools
+from collections import deque
 def solution(n, wires):
     
     #노드 개수  n, 연결 정보
     data = wires
     m = len(wires)
         
-    def find_parent(x):
-        if parent[x] != x:
-            parent[x] = find_parent(parent[x])
-        return parent[x]
-    
-    def union_parent(a,b):
-        a = find_parent(a)
-        b = find_parent(b)
-        if a < b:
-            parent[b] = a
-        else:
-            parent[a] = b
-   #모든 노드 연결 완료
-    
-    res = 242424224 #최솟값
-    #m개의 연결 중 한개 선택해서 최댓값 찾기
-    for i in range(m):
-        #i번째 인덱스 연결 끊었다고 가정
-        parent = [i for i in range(n+1)]
-        for idx, val in enumerate(data):
-            if idx == i:
-                continue
-            a = val[0]
-            b = val[1]
-            union_parent(a,b)
+    g = [[] for _ in range(n+1)]
+    def BFS(v):
+        ch = [0] * (n+1)
+        ch[v] = 1 #시작좌표 
+        dq = deque()
+        dq.append(v)
         
-        for i in range(1,n+1):
-            i = find_parent(i)
-        v = parent[1]
-        cntA=cntB=0
-        for i in range(1,n+1):
-            if parent[i] == v:
-                cntA += 1
-            else:
-                cntB += 1
+        cnt = 1
+        while dq:
+            now = dq.popleft()
+            for node in g[now]: #현재 노드 인접노드들
+                if ch[node] == 0: #방문 전
+                    ch[node] = 1
+                    cnt += 1
+                    dq.append(node)
+        return cnt
+    
+    for i in range(m):
+        a,b = data[i][0], data[i][1]
+        g[a].append(b)
+        g[b].append(a)
+    #그래프 전부 연결해놓고
+    res = 24242424
+    
+    for i in range(m):
+        #한번씩 연결 끊어야지
+        a,b = data[i][0], data[i][1]
+        g[a].remove(b)
+        g[b].remove(a)
+        
+        cntA = BFS(a)
+        cntB = BFS(b)
         temp = abs(cntA-cntB)
-        res = min(temp, res)
-        print(parent)
+        res = min(res, temp)
+        
+        g[a].append(b)
+        g[b].append(a) #다시 연결!
+        
+        
+    # for i in range(m):
+    #     g = [[] for _ in range(n+1)]
+    #     for j in range(m):
+    #         if i == j:
+    #             continue #제외하는 간선
+    #         #i번째 간선을 제외시킴.
+    #         a,b = data[j][0], data[j][1]
+    #         g[a].append(b)
+    #         g[b].append(a)
+    #     #해당 간선 제외하고 그래프 연결 완료
+    #     #끊은 간선의 두 노드에서 연결요소 개수 가져와야함
+    #     a = data[i][0]
+    #     b = data[i][1]
+    #     cntA = BFS(a)
+    #     cntB = BFS(b)
+    #     temp = abs(cntA-cntB)
+    #     res = min(res, temp)
+    
+    
     print(res)
     return res
+        
+        
+        
             
     
