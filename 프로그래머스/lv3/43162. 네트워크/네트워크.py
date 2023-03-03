bdabe4d124b2814f84896511a82eaa408a30862b
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2023-03-04 02:14:18 +0900

    [level 3] Title: 네트워크, Time: 0.48 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,16 +1,31 @@
+from collections import deque
 def solution(n, computers):
-    #n은 노드 개수 computers는 2차원 그래프
-    g = computers
-    ch = [0] * n
+    data = computers
+    print(data)
+    #네트워크 개수 = 연결요소의 개수
     
-    def DFS(v):
+    g = [[] for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if i != j:
+                if data[i][j] == 1:
+                    g[i].append(j)
+    
+    def BFS(v):
         ch[v] = 1
-        for i in range(n):
-            if g[v][i] == 1 and ch[i] == 0:
-                DFS(i)
-    cnt = 0
+        dq = deque()
+        dq.append(v)
+        while dq:
+            now = dq.popleft()
+            for node in g[now]:
+                if ch[node] == 0:
+                    ch[node] = 1
+                    dq.append(node)
+        
+    res = 0
+    ch = [0] * n
     for i in range(n):
         if ch[i] == 0:
-            DFS(i)
-            cnt += 1
-    return cnt
+            BFS(i)
+            res += 1
+    return res            
