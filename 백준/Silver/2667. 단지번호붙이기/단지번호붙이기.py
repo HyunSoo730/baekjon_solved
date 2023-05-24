52986c174e6d5ce29a489c79bb173a0cee38a266
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2023-05-24 17:23:33 +0900

    [Silver I] Title: 단지번호붙이기, Time: 172 ms, Memory: 115592 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,43 +1,44 @@
 import sys
+from collections import deque
+
+
+# 상하좌우 연결요소 구하기 문제
 
 n = int(input())
-g = []
+g = [list(map(int, input())) for _ in range(n)]
 
-for i in range(n):
-    data = list(map(int, input()))
-    g.append(data)
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
-cnt = 0
-cnt_list = []
-res = 0
+def BFS(a,b):
+    dq = deque()
+    dq.append((a,b))
+    g[a][b] = 0 #시작 지점 방문 처리
+    cnt = 1
 
-def DFS(x,y):
-    global cnt
+    while dq:
+        x,y = dq.popleft()
 
-    if g[x][y] == 1:
-        g[x][y] = 0
-        cnt += 1
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0<=nx<n and 0<=ny<n and g[nx][ny] == 1:
-                DFS(nx,ny)
 
+            if 0<=nx<n and 0<=ny<n: #경계선 안
+                if g[nx][ny] == 1:
+                    g[nx][ny] = 0 #방문 처리
+                    cnt += 1
+                    dq.append((nx,ny))
+    return cnt
 
+res = []
+total_cnt = 0
 for i in range(n):
     for j in range(n):
-        if g[i][j] == 1:
-            cnt = 0     
-            DFS(i,j)
-            res += 1
-            cnt_list.append(cnt)
+        if g[i][j] == 1: #집
+            total_cnt += 1
+            res.append(BFS(i,j))
 
-    
-print(res)
-cnt_list.sort()
-for x in cnt_list:
+res.sort()
+print(total_cnt)
+for x in res:
     print(x)
-    
-    
