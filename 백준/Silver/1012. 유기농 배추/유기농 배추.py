33640a77dcbe8768e21415f876713ad5841fbb99
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2023-05-24 17:32:27 +0900

    [Silver II] Title: 유기농 배추, Time: 204 ms, Memory: 116740 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,32 +1,43 @@
 import sys
-sys.setrecursionlimit(10000)
+from collections import deque
 
-dx = [-1,1,0,0]
-dy = [0,0,1,-1]
-def DFS(x,y):
-    #상하좌우 확인
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        
-        if 0<=nx<n and 0<=ny<m:
-            if board[nx][ny] == 1: #배추
-                board[nx][ny] = 0 #초기화.
-                DFS(nx,ny)
 
 T = int(input())
+
+
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
+def BFS(a,b):
+    dq = deque()
+    dq.append((a,b))
+    g[a][b] = 0 #시작 지점 방문 처리
+
+    while dq:
+        x,y = dq.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0<=nx<n and 0<=ny<m:
+                if g[nx][ny] == 1: #방문 전
+                    g[nx][ny] = 0
+                    dq.append((nx,ny))
+
+
+
 for _ in range(T):
-    cnt = 0
-    m,n,k = map(int, input().split()) #가로 세로 배추 개수
-    board = list([0] * m for _ in range(n))
+    m,n,k = map(int, input().split()) #가로, 세로, 위치 개수
+    g = [[0] * m for _ in range(n)]
+
     for _ in range(k):
-        x,y = map(int, input().split()) #가로 세로
-        board[y][x] = 1
-    
+        a,b = map(int, input().split())
+        g[b][a] = 1
+
+    cnt = 0
     for i in range(n):
         for j in range(m):
-            if board[i][j] == 1:
-                DFS(i,j)
+            if g[i][j] == 1: #배추.
+                BFS(i,j)
                 cnt += 1
-    
+
     print(cnt)
