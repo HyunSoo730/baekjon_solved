419ff6974e22fc730e76e7156f2868eb5f3b2464
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2023-05-24 17:13:16 +0900

    [Silver III] Title: 바이러스, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,29 +1,27 @@
 import sys
 
-n = int(input()) #노드 수
-m = int(input()) #간선 수
-g = [[0] * (n+1) for _ in range(n+1)]
 
-for i in range(m):
+n = int(input()) # <= 100
+m = int(input()) #간선
+
+g = [[] for _ in range(n+1)]
+
+for _ in range(m):
     a,b = map(int, input().split())
-    g[a][b] = 1
-    g[b][a] = 1 # 무방향 그래프
-    
+    g[a].append(b)
+    g[b].append(a)
+
 ch = [0] * (n+1)
-ch[1] = 1 #시작 노드 방문 표시 1번 노드부터 시작하니까
 cnt = 0
 def DFS(v):
     global cnt
-    for i in range(1,n+1):
-        if g[v][i] == 1 and ch[i] == 0: #연결되어 있고 방문 전
+    ch[v] = 1
+
+    for node in g[v]:
+        if ch[node] == 0:
+            ch[node] = 1
             cnt += 1
-            ch[i] = 1
-            DFS(i)
+            DFS(node)
 
 DFS(1)
 print(cnt)
-        
-    
-    
-    
-    
