aefb875d0fbe783a40ac1ea84ecbc33d0630a0f5
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2023-11-08 13:00:34 +0900

    [Gold III] Title: 파티, Time: 1268 ms, Memory: 119944 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,56 +1,46 @@
 import sys
 
-
 import heapq
 
-#최단거리. 그래프.
-n,m,x = map(int, input().split()) #간선 노드 도착점
-g = [[] for _ in range(n+1)]
+n,m,x = map(int, input().split())
+g = [[] for _ in range(n+1)] #1번 인덱스부터 사용
 for _ in range(m):
     a,b,c = map(int, input().split())
-    g[a].append((b,c))
+    g[a].append((b,c)) # a번 노드에서 b번 노드로 c만큼 든다
 
-INF = int(1e8)
-def dijkstra(start):
-    distance = [INF] * (n+1)
-    q = [] #큐를 이용하여
-    heapq.heappush(q, (0, start))
-    
+INF = int(1e9)
+dis = [INF] * (n+1)
+# 여기까지 기본세팅
+
+# 다익스트라 알고리즘
+def dijkstra(start): # 시작노드
+    q = [] # 우선순위 큐 사용
+    heapq.heappush(q, (0, start)) # 거리, 노드
+    dis[start] = 0
+    #출발 노드에 대한 세팅 끝
+
     while q:
-        dist, now = heapq.heappop(q)
-        
-        if distance[now] < dist:
+        distance, now = heapq.heappop(q)
+        if dis[now] < distance: # 저장된 값이 더 작으면 돌 이유가 없음
             continue
+
+        #현재 노드와 인접노드들 확인
         for node in g[now]:
-            cost = dist + node[1]
-            
-            if cost < distance[node[0]]:
-                distance[node[0]] = cost
+            cost = node[1] + distance
+            # 현재 노드(now)를 거쳐서 node[0]으로 가는데 걸리는 비용 cost
+            if cost < dis[node[0]]:
+                dis[node[0]] = cost # 갱신
                 heapq.heappush(q, (cost, node[0]))
-                
-    return distance
 
 res = [0] * (n+1)
 for i in range(1,n+1):
-    if i != x:
-        toX = dijkstra(i)  #i에서 출발하는 모든정점까지의 최단거리를 저장하고
-        fromX = dijkstra(x)   #x에서 출발하는 모든 정점까지의 최단거리 저장하고
-        res[i] = toX[x] + fromX[i]
-    #왜 x-> x->x 를 포함하지 않아야 되는가? 단방향이라서 ?
+    dis = [INF] * (n+1)
+    dijkstra(i) # 시작 노드별로 기록 갱신
+    disA = dis[x]
+    dis = [INF] * (n+1)
+    dijkstra(x)
+    disB = dis[i]
+    res[i] = disA + disB
 
-# x->x->x 이 경우가 정답보다 큰 경우가 있을 수도 있다?
-print(max(res))
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-    
+max_val = max(res[1:])
+print(max_val)
