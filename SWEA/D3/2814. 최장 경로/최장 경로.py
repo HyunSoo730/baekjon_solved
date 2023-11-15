1907dd2e5acc5ae764b60f5e3189258b6b9e1305
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2023-11-15 17:30:50 +0900

    [D3] Title: 최장 경로, Time: 136 ms, Memory: 60,068 KB -BaekjoonHub

--- SWEA/D3/2814. 최장 경로/최장 경로.py
+++ SWEA/D3/2814. 최장 경로/최장 경로.py
@@ -1,30 +1,33 @@
 
 
-def dfs(v, sum): # 현재 g에서 최장거리 구하기.
+def dfs(v, sum):
     global res
-    if sum > res: # 먼저 최댓값을 넘었는지 확인.
+    if sum > res:  # 종점이 정해지지 않았기에 계속 비교해서 갱신..??
         res = sum
     else:
-        for node in g[v]: # 인접노드들 하나씩 확인
-            if ch[node] == 0: # 아직 방문 전
-                ch[node] = 1
-                dfs(node, sum + 1)
-                ch[node] = 0 # 백트랙킹 시 반환
-
+        for node in g[v]: # 현재 노드의 인접 노드들 방문
+            if ch[node] == 0:
+                ch[node] = 1  #방문 처리
+                dfs(node, sum + 1) # 이제 해당 노드로 들어가서 다시 인접노드 있는지 탐색
+                ch[node] = 0  # 백트랙킹 시 복구
 
 
 T = int(input())
 for t in range(1,T+1):
-    n,m = map(int, input().split())
-    g = [[] for _ in range(n+1)]
-    res = 0
+    n,m = map(int, input().split()) # 노드, 간선
+    g = [[] for _ in range(n+1)] # 1번 인덱스부터 사용하기 위해.
     for _ in range(m):
         x,y = map(int, input().split())
         g[x].append(y)
         g[y].append(x)
-    ch = [0] * (n+1)
-    for i in range(1,n+1):
-        ch[i] = 1
-        dfs(i, 1)
-        ch[i] = 0
+    # 1~n까지 모두 시작점으로 설정해서 진행해야함.
+    res = 0 # 최장거리를 기록할 변수
+    ch = [0] * (n+1) # 중복체크를 위한 배열
+    for v in range(1,n+1):
+        ch[v] = 1
+        dfs(v, 1)
+        ch[v] = 0
+
     print(f"#{t} {res}")
+
+
