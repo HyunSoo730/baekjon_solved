cbb95576924ee5092098e461e287352e85f6d3fd
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2023-11-15 17:07:42 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문2, Time: 662 ms, Memory: 64,844 KB -BaekjoonHub

--- SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.py
+++ SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.py
@@ -1,43 +1,46 @@
-def palindrome_row(data): # 가로 최대 길이 찾는 회문
+
+
+def palindrome_row(data): # 100x100  가로 확인
     cnt = 0
-    for length in range(100, 0, -1):
+    for length in range(100, 0, -1): # 100부터 1까지 줄여나가면서 최대 찾기
         for i in range(100):
             for j in range(100):
-                if j + length > 100: # 범위를 넘어가면 탈출
+                if j + length > 100: # 범위 벗어날 시 끝
                     break
                 e = j + length -1 # 마지막 인덱스
-                for k in range(j, j + length // 2):
-                    if data[i][k] != data[i][e]:
+                for k in range(j,j+length):
+                    if data[i][k] != data[i][e]: # 회문이 아닐 시
                         break
-                    e -= 1
-                else: # 모두 돌았어 그러면 회문
+                    e -= 1 # 통과할 때마다 끝 인덱스는 줄여나가야지
+                else: # 반복문 모두 돈 경우 : 회문이 맞아
                     cnt = length
-                    return cnt
+                    return cnt # length는 100에서 줄여나가고 있기 때문에 바로 리턴하면 돼
 
-def palindrome_col(data): # 세로 최대 길이 찾는 회문
+def palindrom_col(data): # 100x100 세로 확인
     cnt = 0
     for length in range(100,0,-1):
         for i in range(100):
             for j in range(100):
-                if j + length > 100: # 범위 넘으면 탈출
+                if j + length > 100: # 범위 벗어날 시 끝
                     break
-                e = j + length -1 # 마지막 인덱스
-                for k in range(j, j+length // 2):
-                    if data[k][i] != data[e][i]:
+                e = j + length -1 # 역시 마지막 인덱스로 설정
+                for k in range(j, j+length):
+                    if data[k][i] != data[e][i]: # 회문이 아닌 경우
                         break
                     e -= 1
                 else:
                     cnt = length
                     return cnt
 
-
-
 T = 10
 for t in range(1,T+1):
     n = int(input())
     data = [list(map(str, input())) for _ in range(100)]
-    cnt1 = palindrome_row(data)
-    cnt2 = palindrome_col(data)
-    cnt = max(cnt1, cnt2)
+    res = 0
+    cntA = palindrome_row(data)
+    cntB = palindrom_col(data)
+    res = max(cntA, cntB)
+    print(f"#{t} {res}")
 
-    print(f"#{t} {cnt}")
+
+
