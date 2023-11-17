0293fcd8ce82858191284bca26d31354a71a0c43
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2023-11-17 12:05:00 +0900

    [Silver I] Title: 단지번호붙이기, Time: 140 ms, Memory: 113164 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,44 +1,42 @@
 import sys
+
 from collections import deque
-
-
-# 상하좌우 연결요소 구하기 문제
 
 n = int(input())
 g = [list(map(int, input())) for _ in range(n)]
 
-dx = [0,0,1,-1]
-dy = [1,-1,0,0]
+res = []
 
-def BFS(a,b):
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
+
+def bfs(a,b):
+    cnt = 1
     dq = deque()
     dq.append((a,b))
-    g[a][b] = 0 #시작 지점 방문 처리
-    cnt = 1
+    g[a][b] = 0 # 방문처리
 
     while dq:
         x,y = dq.popleft()
-
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0<=nx<n and 0<=ny<n: #경계선 안
-                if g[nx][ny] == 1:
-                    g[nx][ny] = 0 #방문 처리
-                    cnt += 1
-                    dq.append((nx,ny))
+            if 0<=nx<n and 0<=ny<n and g[nx][ny] == 1: # 좌표 내부이면서 방문 전
+                g[nx][ny] = 0 # 방문처리
+                cnt += 1
+                dq.append((nx,ny))
     return cnt
 
-res = []
-total_cnt = 0
+cnt = 0
 for i in range(n):
     for j in range(n):
-        if g[i][j] == 1: #집
-            total_cnt += 1
-            res.append(BFS(i,j))
-
+        if g[i][j] == 1: # 아직 방문 전
+            cnt += 1 # 연결 요소 개수 추가
+            r = bfs(i,j)
+            res.append(r)
 res.sort()
-print(total_cnt)
-for x in res:
-    print(x)
+
+print(cnt)
+for c in res:
+    print(c)
