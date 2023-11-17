9022c527e8bdc0e2b64378d642e4e99294482fd5
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2023-11-17 12:34:04 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 516 ms, Memory: 177420 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,34 +1,26 @@
 import sys
-sys.setrecursionlimit(10000)
-input = sys.stdin.readline
 
-n, m = map(int, input().split())
-g = [[] for _ in range(n+1)]   #빈 리스트로 했어야함..! 무방향 노드들만 있는 상태니까! 
-ch = [0] * (n+1)
-## 그래프 전체 0으로 채워야 하는 것을 상하좌우 구분 있을 경우고 그 외에는 이런 식으로 빈 리스트에 추가하는 식으로 진행하자.
+from collections import deque
+
+n,m = map(int, input().split())
+g = [[] for _ in range(n+1)] # 1번 인덱스부터 사용
+
 for _ in range(m):
-    u, v = map(int, input().split())
-    g[u].append(v)
-    g[v].append(u)
+    a,b = map(int, input().split())
+    g[a].append(b)
+    g[b].append(a)
 
+ch = [0] * (n+1)
+def dfs(v):
+    ch[v] = 1  #방문처리
+
+    for node in g[v]: # 현재 노드의 인접 노드들
+        if ch[node] == 0: #방문 전
+            ch[node] = 1 # 방문처리 후 들어감
+            dfs(node)
 cnt = 0
-def DFS(v):
-    ch[v] = 1 #방문 표시
-    for x in g[v]: #인접노드들
-        if ch[x] == 0:
-            DFS(x)
-    
 for i in range(1,n+1):
-    if ch[i] == 0: #방문 전
-        DFS(i)
+    if ch[i] == 0:
+        dfs(i)
         cnt += 1
-
 print(cnt)
-
-
-        
-        
-        
-        
-        
-        
