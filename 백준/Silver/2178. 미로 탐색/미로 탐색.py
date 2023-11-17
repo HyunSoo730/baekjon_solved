edbc9ee2004e5b933203b116b1dafc2a96c75ded
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2023-11-17 10:39:50 +0900

    [Silver I] Title: 미로 탐색, Time: 172 ms, Memory: 114132 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,38 +1,30 @@
 import sys
-# sys.stdin = open("input.text", "rt")
+
 from collections import deque
 
 n,m = map(int, input().split())
-#최단거리
 g = [list(map(int, input())) for _ in range(n)]
-
-#1로만 이동
-dis = [[0] * m for _ in range(n)]
-dis[0][0] = 1 #시작위치는 1
-
+dis = [[0] * m for _ in range(n)] # 최단거리를 기록할 리스트
+dis[0][0] = 1 # 시작지점부터 1
 dx = [1,-1,0,0]
-dy = [0,0,1,-1]
+dy = [0,0,1,-1]  # 방향 벡터 설정
 
-def BFS(a,b):
+def bfs(a,b): # 시작 위치 a,b
     dq = deque()
     dq.append((a,b))
-    g[a][b] = 0 #방문처리
+    g[a][b] = 0 # 시작노드 방문 처리. 0,1로 나타내기에 g 내에서 그냥 방문처리 가능
 
     while dq:
         x,y = dq.popleft()
-       
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
+            if 0<=nx<n and 0<=ny<m and g[nx][ny] == 1: # 좌표 내부이면서, 아직 방문 전
+                g[nx][ny] = 0 #방문 처리 후
+                dis[nx][ny] = dis[x][y] + 1 # 거리 갱신
+                dq.append((nx,ny))
 
-            if 0<=nx<n and 0<=ny<m:
-                if g[nx][ny] == 1: #방문 전
-                    g[nx][ny] = 0 #방문처리
-                    dis[nx][ny] = dis[x][y] + 1
-                    dq.append((nx,ny))
-
-    
-BFS(0,0)
-
-
+bfs(0,0)
 print(dis[n-1][m-1])
+
+
