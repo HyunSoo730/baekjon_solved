9d537267c91301d373f95dd26b6a1652baaa1f08
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2023-11-19 00:20:22 +0900

    [Silver I] Title: 영역 구하기, Time: 152 ms, Memory: 114268 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,42 +1,42 @@
 import sys
+from collections import deque
 
-input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
-
-m,n,k = map(int, input().split()) #m 가로 n 세로 k 직사각형 개수
+m,n,k = map(int, input().split()) # 세로, 가로
 g = [[0] * n for _ in range(m)]
 for _ in range(k):
-    x1,y1,x2,y2 = map(int, input().split())
-    for i in range(m-y2,m-y1):
-        for j in range(x1,x2):
+    a,b,c,d = map(int, input().split()) # n,m으로 주어짐; (가로, 세로)
+    for i in range(b,d):
+        for j in range(a,c):
             g[i][j] = 1
-
 
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
 
-def DFS(x,y):
-    global cnt
-    g[x][y] = 1 #방문 처리
-
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-
-        if 0<=nx<m and 0<=ny<n:
-            if g[nx][ny] == 0:
-                cnt += 1
-                DFS(nx,ny)
+cnt = 0
 res = []
+
+def bfs(a,b):
+    count = 1 # 시작지점 포함
+    g[a][b] = 1 # 시작지점 방문 표시
+    dq = deque()
+    dq.append((a,b))
+
+    while dq:
+        x,y = dq.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y  +dy[i]
+            if 0<=nx<m and 0<=ny<n and g[nx][ny] == 0: # 아직 방문 가능
+                g[nx][ny] = 1
+                count += 1
+                dq.append((nx,ny))
+    return count
+
 for i in range(m):
     for j in range(n):
-        if g[i][j] == 0: #구역.
-            cnt = 1 
-            DFS(i,j)
-            res.append(cnt)
-
-print(len(res))
+        if g[i][j] == 0:
+            cnt += 1
+            res.append(bfs(i,j))
+print(cnt)
 res.sort()
-for x in res:
-    print(x, end = " ")
-
+print(*res)
