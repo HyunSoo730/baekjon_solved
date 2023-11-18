75b70ff78ace78a5e8f93ad0772850febe7fc1f7
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2023-11-19 01:30:33 +0900

    [Silver III] Title: N과 M (3), Time: 740 ms, Memory: 155524 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,18 +1,14 @@
 import sys
+from collections import deque
 
 n,m = map(int, input().split())
-#중복 가능.
-#중복 순열
-
 res = []
-def DFS(L):
-    if L == m: #모두 선택. 종료조건
-        for x in res:
-            print(x, end = " ")
-        print()
+def dfs(L):
+    if L == m: # m개 뽑음. 종료조건
+        print(*res)
     else:
         for i in range(1,n+1):
             res.append(i)
-            DFS(L+1)
+            dfs(L+1)
             res.pop()
-DFS(0)
+dfs(0)
