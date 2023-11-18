093a51d972b7a70eb08f3a0cfb8e55fb82dd490f
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2023-11-19 00:42:12 +0900

    [Silver II] Title: 유기농 배추, Time: 184 ms, Memory: 113972 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,43 +1,34 @@
 import sys
 from collections import deque
 
-
-T = int(input())
-
-
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
-def BFS(a,b):
+
+def bfs(a,b):
+    g[a][b] = 0 # 시작지점 방문처리
     dq = deque()
     dq.append((a,b))
-    g[a][b] = 0 #시작 지점 방문 처리
 
     while dq:
         x,y = dq.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
+            if 0<=nx<n and 0<=ny<m and g[nx][ny] == 1: # 좌표 내부이면서 방문 전
+                g[nx][ny] = 0 # 방문처리
+                dq.append((nx,ny))
 
-            if 0<=nx<n and 0<=ny<m:
-                if g[nx][ny] == 1: #방문 전
-                    g[nx][ny] = 0
-                    dq.append((nx,ny))
-
-
-
+T = int(input())
 for _ in range(T):
-    m,n,k = map(int, input().split()) #가로, 세로, 위치 개수
+    m,n,k = map(int, input().split())
     g = [[0] * m for _ in range(n)]
-
     for _ in range(k):
         a,b = map(int, input().split())
         g[b][a] = 1
-
     cnt = 0
     for i in range(n):
         for j in range(m):
-            if g[i][j] == 1: #배추.
-                BFS(i,j)
+            if g[i][j] == 1:
                 cnt += 1
-
+                bfs(i,j)
     print(cnt)
