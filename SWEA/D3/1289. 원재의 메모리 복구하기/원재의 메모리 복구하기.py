328c294a9713b0532b81705971ae6689d50e56fa
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2024-01-29 17:26:24 +0900

    [D3] Title: 원재의 메모리 복구하기, Time: 101 ms, Memory: 43,500 KB -BaekjoonHub

--- SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.py
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.py
@@ -1,29 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Solution {
 
-T = int(input())
-for t in range(T):
-    data = list(map(int, input()))
-    # 오른쪽에서부터 카운트
-    cnt = 0
-    n = len(data)
-    idx = n-1
-    while True:
-        x = data[idx]
-        for i in range(idx, -1, -1):
-            if data[idx] != data[i]:
-                idx = i
-                break
-        else:
-            if x == 1:
-                cnt += 1
-                break
-            else:
-                break
-        cnt += 1
-        if x == 0:
-            for i in range(idx+1, n):
-                data[i] = 1
-        else:
-            for i in range(idx+1, n):
-                data[i] = 0
-    print(f"#{t+1} {cnt}")
+    //원래 메모리가 무슨 값이었는지 알고 있음.
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int T;
+    static int[] data;
+
+    public static void main(String[] args) throws IOException {
+        T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
+            String memory = br.readLine();
+            data = new int[memory.length()];
+            for (int i = 0; i < data.length; i++) {
+                data[i] = Character.getNumericValue(memory.charAt(i));
+            }
+            // 해당 숫자 확인해서 카운팅
+            int cnt = data[0];  // 시작지점이 1이면 카운트하고 시작
+            int last = data[0];
+            for (int i = 0; i < data.length; i++) {
+                if (last != data[i]) {
+                    cnt += 1;
+                    last = data[i];
+                }
+            }
+            System.out.println("#" + t + " " + cnt);
+        }
+    }
+}
