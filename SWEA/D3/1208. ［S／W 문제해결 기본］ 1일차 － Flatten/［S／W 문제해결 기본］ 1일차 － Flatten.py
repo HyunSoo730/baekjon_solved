98a132d438685bf416f3ba31d3aa6906c60b342e
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2024-01-30 13:50:41 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 124 ms, Memory: 58,836 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
@@ -1,12 +1,44 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-T = 10
-for i in range(T):
-    n = int(input()) # 덤프 횟수
-    data = list(map(int, input().split()))
-    for _ in range(n):
-        max_idx = data.index(max(data))
-        min_idx = data.index(min(data))
-        data[max_idx] -= 1
-        data[min_idx] += 1
-    print(f"#{i+1} {max(data) - min(data)}")
-    
+public class Solution {
+
+    static int[] data;
+    static int n; // 덤프 횟수
+    static int MAX_size = 100;
+
+    static int T = 10;
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        for (int t = 1; t <= T; t++) {
+            n = Integer.parseInt(br.readLine()); // 덤프 횟수
+            st = new StringTokenizer(br.readLine());
+            data = new int[100];
+            for (int i = 0; i < 100; i++) {
+                data[i] = Integer.parseInt(st.nextToken());
+            }
+            int cnt = cal(data, n);
+            System.out.println("#" + t + " " + cnt);
+
+        }
+    }
+
+    public static int cal(int[] data, int dump) {
+        Arrays.sort(data);
+        for (int i = 0; i < dump; i++) {
+            if (data[MAX_size - 1] - data[0] <= 1) {
+                break;
+            }
+            data[0] += 1;
+            data[MAX_size-1] -=1;
+            Arrays.sort(data);
+        }
+        return data[MAX_size-1] - data[0];
+    }
+}
