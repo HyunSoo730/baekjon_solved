60feac402a540b843ce4a666b8ca063c0d7c87e3
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2024-02-05 15:51:53 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 412 ms, Memory: 246356 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -2,15 +2,13 @@ import sys
 from collections import deque
 
 
-# n , k
 n,k = map(int, input().split())
-
 dq = deque(range(1,n+1))
-
 res = []
+
 while dq:
-    for _ in range(k-1):
+    for i in range(k-1):
         dq.append(dq.popleft())
     res.append(dq.popleft())
 
-print(str(res).replace('[', '<').replace(']', '>'))
+print("<" + ", ".join(map(str, res)) + ">")
