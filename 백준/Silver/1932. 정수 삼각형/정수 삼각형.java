2a3bf297ab3e4344f18fcc0760b91f8973bd4e1b
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2024-02-21 11:29:24 +0900

    [Silver I] Title: 정수 삼각형, Time: 280 ms, Memory: 27060 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -10,38 +10,37 @@ public class Main {
 
     static int n;
     static int[][] data, dp;
+
     // ! 아래층에 있는 수는 현재 층에서 선택된 수의 대각선 왼쪽 또는 대각선 오른쪽에 있는 것 중에서만 선택 가능
+    // ! 굳이 규격에 맞게 배열을 만들 필요가 없었음 -> [n+1][n+1]로 만들어도 충분하다.
     public static void main(String[] args) throws IOException {
+
         n = Integer.parseInt(br.readLine());
         // 현재의 선택이 나중의 선택에 영향을 끼침 -> dp
-        data = new int[n][];
-        dp = new int[n][];
-        for (int i = 0; i < n; i++) {
-            data[i] = new int[i + 1];
-            dp[i] = new int[i + 1];
-        }
+        data = new int[n+1][n+1];
+        dp = new int[n+1][n+1];
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j <= i; j++) {
-                int val = Integer.parseInt(st.nextToken());
-                data[i][j] = val;
+            for (int j = 1; j <= i; j++) {
+                data[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        dp[0][0] = data[0][0];  // 자명한 값
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                if(j == 0) dp[i][j] = dp[i - 1][j] + data[i][j];
-                else if(j == i) dp[i][j] = dp[i - 1][j - 1] + data[i][j];
-                else dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]) + data[i][j];
+        dp[1][1] = data[1][1];  // 자명한 값
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (j == 1) dp[i][j] = dp[i - 1][j] + data[i][j];
+                else if (j == i) dp[i][j] = dp[i - 1][j - 1] + data[i][j];
+                else dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + data[i][j];
             }
         }
 
         int res = 0;
-        for (int i = 0; i < n; i++) {
-            res = Math.max(res, dp[n - 1][i]);
+        for (int i = 1; i <= n; i++) {
+            res = Math.max(res, dp[n][i]);
         }
         System.out.println(res);
+//        System.out.println(Arrays.toString(dp[n]));
     }
 }
