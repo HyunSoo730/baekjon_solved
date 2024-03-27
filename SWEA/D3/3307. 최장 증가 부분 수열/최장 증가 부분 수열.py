f36f0172f3d1c3104a8aec70cd8855fae5a4e998
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2024-03-27 12:11:57 +0900

    [D3] Title: 최장 증가 부분 수열, Time: 159 ms, Memory: 62,236 KB -BaekjoonHub

--- SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.py
+++ SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.py
@@ -1,14 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-T = int(input())
-for t in range(1,T+1):
-    n = int(input())
-    data = [0] + list(map(int, input().split()))
+public class Solution {
 
-    dp = [0] * (n+1)
-    for i in range(1,n+1): # ! n개 모두 확인
-        max_length = 0
-        for j in range(i-1, 0, -1): # * i-1 ~ 1번까지 순차적으로 확인
-            if data[i] > data[j] and dp[j] > max_length:
-                max_length = dp[j]
-        dp[i] = max_length + 1
-    print(f"#{t} {max(dp)}")
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static int T;
+    static int n;
+    static int[] data, dp;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+
+        T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
+            n = Integer.parseInt(br.readLine());
+            data = new int[n + 1];
+            st = new StringTokenizer(br.readLine());
+            for (int i = 1; i <= n; i++) {
+                data[i] = Integer.parseInt(st.nextToken());
+            }
+
+            dp = new int[n+1];
+
+            for (int i = 1; i <= n; i++) {
+                int max_length = 0;
+                for (int j = i - 1; j >= 1; j--) {
+                    if (data[i] > data[j] && dp[j] > max_length) {
+                        max_length = dp[j];
+                    }
+                }
+                dp[i] = max_length + 1;
+            }
+            int res = 0;
+            for (int i = 1; i <= n; i++) {
+                res = Math.max(res, dp[i]);
+            }
+            sb.append("#").append(t).append(" ");
+            sb.append(res).append("\n");
+
+        }
+        System.out.println(sb);
+    }
+}
