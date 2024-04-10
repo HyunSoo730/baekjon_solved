79587b997b095c86aafd89109bda64ec40e352e2
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2024-04-10 22:35:21 +0900

    [Unrated] Title: [모의 SW 역량테스트] 디저트 카페, Time: 1,827 ms, Memory: 83,328 KB -BaekjoonHub

---
+++ SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/README.md
@@ -0,0 +1,15 @@
+# [Unrated] [모의 SW 역량테스트] 디저트 카페 - 2105 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5VwAr6APYDFAWu) 
+
+### 성능 요약
+
+메모리: 83,328 KB, 시간: 1,827 ms, 코드길이: 1,642 Bytes
+
+### 제출 일자
+
+2024-04-10 22:35
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.py
@@ -0,0 +1,50 @@
+
+# nxn 보드, 각 칸의 값은 디저트 종류
+# 대각선 방향으로 움직이고 사각형 모양을 그리며 출발한 카페로 돌아와야 한다.
+# 최대한 많이 먹고 오려고 한다.
+# 왔던 길 다시 못감
+# 같은 숫자의 디저트를 팔고 있는 카페가 있으면 안된다.
+# 임의의 한 카페에서 출발해 대각선 방향으로 움직이고 서로 다른 디저트를 먹으면서 사각형 모양을 그리며 출발점으로 돌아오는 경우
+# 디저트를 가장 많이 먹을 수 있는 경로를 찾고, 그 때의 디저트 수를 출력
+# 그 경우 없으면 -1
+
+dx = [1,1,-1,-1]
+dy = [-1,1,1,-1] # 순서대로 방향
+
+def isInner(x,y):
+    if 0<=x<n and 0<=y<n:
+        return True
+    return False
+
+def DFS(L, dir,x,y,visited): # 현재 방향도 함께. 현재 방향 기준 직진(방향 유지), 좌회전을 해야하기 때문.
+    global res
+    if dir > 3: # 무조건 종료 방향은 3번 바꾸면 끝 가지치기 더 바꾸면 끝내야함.
+        return
+    if dir == 3 and (i,j) == (x,y): # 돌아온 경우
+        res = max(res, L)
+    else:
+        for d in range(2): # 방향 2개만 고려해도 됨
+            direction = (dir + d) % 4 # 없을 수도 있으니 일단은 이렇게 진행.
+            nx = x + dx[direction]
+            ny = y + dy[direction]
+            if isInner(nx,ny) and g[nx][ny] not in visited:
+                visited.append(g[nx][ny])
+                DFS(L+1, dir + d, nx,ny,visited)
+                visited.pop() # 백트랙킹 시 원상복구
+
+
+
+
+T = int(input())
+for t in range(1,T+1):
+    n = int(input())
+    g = [list(map(int, input().split())) for _ in range(n)]
+
+    res = -1
+    for i in range(n):
+        for j in range(n):
+            # 시작좌표는 다시 돌아와야 하므로 ! 방문처리하지 않아.
+            visited = [[False] * n for _ in range(n)]
+            DFS(0,0,i,j,[])
+
+    print(f"#{t} {res}")
