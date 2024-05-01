5c5e69a93927af77d0a56b7cee11c918004bba39
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2024-05-02 00:30:09 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 311 ms, Memory: 57,044 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.py
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.py
@@ -1,98 +1,82 @@
 
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+# nxn 보드
+# 각 칸은 셀이라고 부름. 각 칸은 1개의 코어 혹은 전선이 올 수 있다.
+# 보드의 가장자리에는 전원이 흐른다
+# 코어와 전원을 연결하는 전선은 직선으로만 설치가 가능, 상하좌우 , 교차 안된다
+# 전원 연결 위해서 가장자리로 코어와 전선 연결해야함
+# 가장자리 코어 전원 연결된 것으로 간주,
+# 최대한 많은 코어에 전원을 연결했을 경우 ,전선 길이의 합 구하기
+# 전선 길이의 합이 최소가 되는 전선 길이의 합 구하기
 
+# 0 빈 셀, 1은 코어
 
-def findCores():
-    global core
-    for i in range(n):
-        for j in range(n):
-            if i == 0 or i == n - 1 or j == 0 or j == n - 1:
-                continue
-            if g[i][j] == 1:
-                core.append((i, j))  # 코어 좌표 저장
-
-
-# def DFS(L, useCnt, length): # ! 현재 인덱스, 코어 사용 개수, 길이
-#     if L == coreCnt: # 모든 경우 확인
-#         pass
-#     else:
-
-def calCoreLength():
-    global coreLength
-    for i in range(coreCnt):
-        for dir in range(4):
-            x, y = core[i]  # ! 코어 위치
-            nx, ny = x, y
-            while True:
-                nx = x + dx[dir]
-                ny = y + dy[dir]
-                if isInner(nx, ny):
-                    if g[nx][ny] == 1:  # 갈 수 없는 경우
-                        coreLength[i][dir] = 0
-                        break
-                else:
-                    coreLength[i][dir] = abs(core[i][0] - x) + abs(core[i][1] - y)
-                    break
-                x, y = nx, ny
-
-
-def DFS(L, useCnt, length):
-    global maxCore, minLength, coreCnt
-    if L == coreCnt:
-        if useCnt > maxCore:
-            maxCore = useCnt
-            minLength = length
-        elif useCnt == maxCore:
-            minLength = min(minLength, length)
-    else:
-        for i in range(4):  # ! 각 코어별 해당 방향 이동 가능한지 체크
-            x, y = core[L]  # 현재 코어의 위치
-            if canConnect(x, y, i):
-                drawLine(x, y, i, 1)  # 해당 라인 체크 후
-                DFS(L + 1, useCnt + 1, length + coreLength[L][i])  # ! 해당 방향 사용
-                drawLine(x, y, i, 0)  # ! 백트랙킹 시 원상 복구
-        DFS(L + 1, useCnt, length)  # ! 역시 해당 코어 사용 안하는 경우도 생각
-
-
-def drawLine(x, y, dir, val):
-    # ! 전선을 그리거나 지우는 용도
-    while True:
-        x += dx[dir]
-        y += dy[dir]
-        if isInner(x, y):
-            g[x][y] = val
-        else:
-            break
-
-
-def canConnect(x, y, dir):  # ! 해당 코어의 현재 위치에서 해당 방향으로 전선을 놓을 수 있는지 확인
-    while True:
-        x += dx[dir]
-        y += dy[dir]
-        if not isInner(x, y):
-            return True
-        if g[x][y] != 0:
-            return False
-
-
-def isInner(x, y):
-    if 0 <= x < n and 0 <= y < n:
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
+def isInner(x,y):
+    if 0<=x<n and 0<=y<n:
         return True
     return False
-
+def DFS(L, cores,length,g): # 현재 몇번째 코어인지, 현재까지의 길이
+    global max_cores, min_length
+    if max_cores > cores + (core_cnt - L): # 최대로 연결된 코어수 > 현재 연결 코어수  + 남은 코어수
+        return
+    if L == core_cnt:  # 모두 확인 -> 최소값인지 계산
+        if cores == max_cores:
+            min_length = min(min_length, length)
+        elif cores > max_cores:
+            max_cores = cores
+            min_length = length
+    else: # 아니라면 현재 코어의 상하좌우 중 전원 연결 가능하다면 연결 후 넘기기
+        # print(f"현재 코어 : {L}, 코어 위치 : {data[L][0], data[L][1]}")
+        for i in range(4):
+            # copy_g = [arr[:] for arr in g] # 배열 복사
+            if can_draw(L, i, g): # 배열 복사가 아닌...
+                # print(f"현재 {L}코어 방향{i}는 라인 그릴 수 있음")
+                line_cnt = draw_line(L, i, g,1) # i방향으로 그리기 가능하면 그리기
+                # 보드출력(copy_g)
+                DFS(L+1, cores+1, length+line_cnt, g)
+                draw_line(L,i,g,0)
+            else:
+                # print(f"현재 {L}코어 방향{i}는 라인 못그림")
+                # 보드출력(copy_g)
+                DFS(L+1, cores,length, g)
+def can_draw(L, dir, g): # L번째 코어, dir방향으로 그리기 가능한지
+    x,y = data[L] # 현재 코어의 위치
+    while True:
+        x += dx[dir]
+        y += dy[dir]
+        if not isInner(x,y):
+            return True # 그을 수 있음
+        if g[x][y] == 1: return False
+def draw_line(L, dir, g, val):
+    x,y = data[L] # 현재 코어의 dir 방향으로 긋기
+    # print(f"현재 코어:{L}, 현재 코어 위치:{x,y} 그리는 방향 : {dir}")
+    cnt = 0
+    while True:
+        x += dx[dir]
+        y += dy[dir]
+        if not isInner(x,y):
+            return cnt
+        g[x][y] = val
+        cnt += 1
+def 보드출력(g):
+    print("=======현재 보드 출력========")
+    for i in range(n):
+        for j in range(n):
+            print(g[i][j], end = " ")
+        print()
 
 T = int(input())
-for t in range(1, T + 1):
+for t in range(1,T+1):
     n = int(input())
     g = [list(map(int, input().split())) for _ in range(n)]
-
-    core = []
-    findCores()
-    coreCnt = len(core)  # ! 코어 개수
-    coreLength = [[0] * 4 for _ in range(coreCnt)]  # !각 코어 별 4방 가능한 경우 길이 미리 계산
-    calCoreLength()
-    maxCore = 0
-    minLength = int(1e9)
-    DFS(0, 0, 0)
-    print(f"#{t} {minLength}")
+    data = []
+    for i in range(1,n-1):
+        for j in range(1,n-1):
+            if g[i][j] == 1:
+                data.append((i,j)) # 코어 위치 저장
+    core_cnt = len(data)
+    max_cores = 0
+    min_length = int(1e9)
+    DFS(0,0,0,g)
+    print(f"#{t} {min_length}")
