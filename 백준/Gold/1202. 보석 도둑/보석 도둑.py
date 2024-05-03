e9455362eb35015cb6d526458f89d1e4e4bab33c
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2024-05-03 10:50:17 +0900

    [Gold II] Title: 보석 도둑, Time: 1024 ms, Memory: 185716 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.py
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -1,31 +1,32 @@
 import heapq
 import sys
+from collections import deque
 
-# 보석 총 n개, 각 보석은 무게m 가격v, 가방을 k개 가지고 있고 각 가방에 담을 수 있는 최대 무게 c
-# 가방에 최대 한개의 보석만.
-# 최대 가격 구하기.
 
-# 하나의 보석이 하나의 가방에 대해서만 가능. -> 최대로 고른다면. 이후의 선택에 영향을 미치지 않음.
-# n,k <= 300,000 -> 우선순위 큐 사용 ?
-n,k = map(int, input().split())
+# 보석 털기
+# 보석 총 N개, 각 보석 무게 m과 가격 v
+# k개의 가방. 각 가방에 담을 수 있는 최대 무게 c
+# 가방에는 한개의 보석만 넣을 수 있다.
+# 훔칠 수 있는 보석의 최대 가격 구하기.
+n, k = map(int, input().split())  # 보석 개수, 가방 개수
 data = []
 for _ in range(n):
-    a,b = map(int, input().split()) # 무게, 가격
-    heapq.heappush(data, (a,-b)) # 우선적으로 무게를 기준 오름차순, 가격 기준 내림차순
+    m, v = map(int, input().split())
+    data.append((m, v))  # 각 보석의 무게, 가격
+
 bags = []
-for _ in range(k):
-    bags.append(int(input())) # 각 가방의 최대 무게
+for _ in range(k):  # k개 가방의 최대 무게
+    bags.append(int(input()))  # 가방의 최대 무게
 
-bags.sort() # 가방의 최대 무게는 오름차순
+bags.sort() # 가장 최대 무게 오름차순 정렬
+data.sort(key = lambda x : x[0]) # 보석을 무게 순으로 오름차순 정렬
 res = 0
-temp = []
-
-for i in range(k):
-    max_weight = bags[i]
-    while data and data[0][0] <= max_weight:
-        weight, value = heapq.heappop(data)
-        heapq.heappush(temp, (value, weight)) # 이미 음수로 넣어뒀기 때문에 그냥 진행
-    if temp: # 들어가 있다면 진행
-        v,w = heapq.heappop(temp) # 가능한 애들 중에 가치가 가장 큰 애를 넣어줌.
-        res += -v
+heap = []
+dq = deque(data)
+for weight in bags: # 각 가방의 최대 무게 하나씩 꺼내서
+    while dq and dq[0][0] <= weight: # 현재 가방의 최대 무게보다 작다면
+        m,v = dq.popleft() # 꺼낸 보석의 무게, 가격
+        heapq.heappush(heap, -v) # 보석의 가격 최대 힙으로
+    if heap: # 꺼낸 보석들이 존재한다면 그 중 최대 가치 꺼내야함
+        res += -heapq.heappop(heap)
 print(res)
