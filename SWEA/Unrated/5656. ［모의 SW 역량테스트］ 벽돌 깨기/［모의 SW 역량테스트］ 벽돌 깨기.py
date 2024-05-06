a799e031ba75548725c116ed4214d97dd713456a
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2024-05-06 13:54:10 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벽돌 깨기, Time: 871 ms, Memory: 66,608 KB -BaekjoonHub

--- SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.py
+++ SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.py
@@ -1,100 +1,90 @@
 from collections import deque
 
-# 벽돌깨기
-# 1. 조건
-# 1-1. 구슬 n번 쏠 수 있음
-# 1-1-1. 구슬은 좌우로 움직일 수 있고, 항상 맨 위에 있는 벽돌만 깬다.
-# 1-1-2. 벽돌의 값만큼의 범위가 제거된다. 1은 본인만, 3이면 본인 포함 + 2길이의 상하좌우 제거
-# 1-2. 벽돌 제거 시 벽돌은 연쇄 제거
-# 1-3. 해당 구슬로 인해 제거할 수 있는 모든 벽돌 제거 후 벽돌을 아래로 내려야 함(중력작용)
 
-# 2. 풀이
-# 필요 기능 : 구슬은 좌우로 이동할 수 있다 -> 매 번 모든 열 중에 한 곳으로 떨어짐. 순서 의미 있고 중복 가능(같은 선택 가능) -> 중복 순열
-# 벽돌 연쇄 제거 기능
-# 벽돌 제거 후 중력작용을 통해 내려야 한다.
+# 벽돌 깨기
+# 구슬 n번 쏠 수 있음. 구슬은 좌우로만 움직일 수 있고, 항상 맨 위에 있는 벽돌만 깬다.
+# 각 칸의 값은 벽돌의 크기, 벽돌 크기 - 1만큼 상하좌우 제거
+# 벽돌은 연쇄적으로 터진다.
+# 벽돌 제거 후 중력작용
+# 벽돌 깨기 이후에 남은 벽돌의 개수를 구하는데, 최소로 남게끔 구하기
+
+# 1. 벽돌 떨어뜨릴 위치 정하기 (중복 순열) -> 어디든지 가능 , 순서 중요
+# 2. 떨어뜨릴 위치 정하면 벽돌 떨어뜨리기
+# 3. 연쇄 작용으로 벽돌 제거
+# 4. 가능한 모든 벽돌 제거 후 중력 작용. (아래로 떨어뜨려야함)
+# 총 n번 반복하기
 
 dx = [-1,0,1,0]
 dy = [0,1,0,-1]
 def isInner(x,y):
-    if 0<=x<h and 0<=y<w:
+    if 0<=x<n and 0<=y<m:
         return True
     return False
-
-def BFS(a,b,power,g):
+def BFS(startX,startY,power, g):
     dq = deque()
-    dq.append((a,b,power)) # 해당 좌표부터 벽돌 깨기 (범위 같이 받음)
-    g[a][b] = 0 # 시작좌표 벽돌 깨고 시작
-
+    dq.append((startX,startY,power)) # 현재 퍼져야 할 크기 함께 저장
+    g[startX][startY] = 0 # 시작점 꺤걸로 시작.
     while dq:
         x,y,p = dq.popleft()
-        for d in range(1,p): # 1인 경우 그냥 끝내버리기 위해
-            for i in range(4):
+        for i in range(4):
+            for d in range(p): # p-1만큼 퍼져나가야 하므로
                 nx = x + dx[i] * d
                 ny = y + dy[i] * d
-                if isInner(nx,ny) and g[nx][ny] > 0: # 벽돌 존재 -> 깨기
-                    dq.append((nx,ny,g[nx][ny])) # 해당 좌표 깼으니 그 좌표에서 다시 깨기
+                if not isInner(nx,ny): continue
+                if g[nx][ny] > 0: # 벽돌 깨야함
+                    dq.append((nx,ny,g[nx][ny]))
                     g[nx][ny] = 0
 
-
-def destroy(y, g): # y열에 폭탄 떨어뜨림
-    for x in range(h):
-        if g[x][y] > 0: # 처음으로 만나는 벽돌
+def 벽돌깨기(y,g): # 현재 보드의 y번쨰 열에서 구슬 떨어뜨림
+    for x in range(n):
+        if g[x][y] > 0: # 벽돌 깨기 시작
             BFS(x,y,g[x][y], g)
-            return
+            break
 
-def recover(g):
-    for y in range(w):
-        temp = []
-        for x in range(h):
-            if g[x][y] > 0:
-                temp.append(g[x][y]) 
-        new_list = [0] * (h-len(temp)) + temp # 중력작용을 위한 작용. 위에서부터 0으로 채워야 하니까
-        for i in range(h):
-            g[i][y] = new_list[i] # 중력 작용
-            
-def DFS(L, g):
+def 벽돌떨어뜨리기(L,g):
     global res
-    if L == n: # n번 모두 떨어뜨림 -> 종료조건
-        cnt = count_board(g)
+    if L == count: # 모두 떨어뜨림 -> 카운트 해야함
+        cnt = 남은벽돌개수(g)
         res = min(res, cnt)
-        # print("최종 보드")
-        # print_board(g)
-        # print(f"최종 보드에 남은 개수 = {cnt}")
-
-    else: # 아직 다 떨어뜨리지 않은 경우 떨어뜨려
-        for i in range(w): # i번쨰 열에 떨어뜨림 -> 안되는 경우에는 백트랙킹 -> 원상복구해야함
-            copy_g = [arr[:] for arr in g] # 배열 복사
-            # print("현재 깨기 전")
-            # print_board(copy_g)
-            # print("깬 후 보드")
-            destroy(i,copy_g) # 벽돌깨기
-            # print_board(copy_g)
-            # print("리커버리 후 보드")
-            recover(copy_g) # 중력작용
-            # print_board(copy_g)
-            DFS(L+1, copy_g) # 백트랙킹 시 어차피 매번 배열 복사하므로 알아서 원상복구 됨
-            # 이 문제는 전체 한 칸만 되돌리는게 아니라, 전체 보드를 되돌려야 하므로 배열 복사하는게 맞아.
-            # 근데 다른 문제들 중에 딱 한 칸에 대해서만 원상복구를 해야하는 경우는 백트랙킹 시 그 선택에 대해서만 원상복구하면 돼
-
-def count_board(g):
+    else: # 아직이라면 떨어뜨리기
+        for y in range(m): # 열 만큼 돌아야함
+            copy_g = [arr[:] for arr in g] # 배열 복사 -> 영향을 끼치기 때문에 배열 복사하면서 진행
+            # print("벽돌 깨기 전 보드")
+            # 보드출력(copy_g)
+            벽돌깨기(y,copy_g)
+            # print("벽돌 꺠고난 후 보드")
+            # 보드출력(copy_g)
+            중력작용(copy_g)
+            # print("중력작용 후 보드")
+            # 보드출력(copy_g)
+            벽돌떨어뜨리기(L+1, copy_g)
+def 중력작용(g):
+    for y in range(m):
+        temp = []
+        for x in range(n):
+            if g[x][y] > 0:
+                temp.append(g[x][y])
+        new_list = [0] * (n-len(temp)) + temp # 중력작용 적용 후
+        for i in range(n):
+            g[i][y] = new_list[i]
+def 남은벽돌개수(g):
     cnt = 0
-    for x in range(h):
-        for y in range(w):
-            if g[x][y] >0:
+    for i in range(n):
+        for j in range(m):
+            if g[i][j] > 0:
                 cnt += 1
     return cnt
-
-def print_board(g):
-    print("현재 보드 출력")
-    for x in g:
-        print(*x)
+def 보드출력(g):
+    print("=========현재 보드 상황=============")
+    for i in range(n):
+        for j in range(m):
+            print(g[i][j], end = " ")
+        print()
 
 T = int(input())
 for t in range(1,T+1):
-    n, w, h = map(int, input().split())  # n번 구슬 떨어뜨리는데, wxh 보드에
-    g = [list(map(int, input().split())) for _ in range(h)]
-    # step1 : 중복 순열 내에서 조작
+    count, m,n = map(int, input().split()) # count만큼 벽돌 떨어뜨리기, 가로,세로
+    g = [list(map(int, input().split())) for _ in range(n)]
     res = int(1e9)
-    DFS(0, g)
+    벽돌떨어뜨리기(0,g)
     print(f"#{t} {res}")
-
