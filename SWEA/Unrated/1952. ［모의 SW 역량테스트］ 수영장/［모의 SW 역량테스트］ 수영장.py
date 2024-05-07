06ebe495a50f485654cfb449935f612aae0d100b
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2024-05-07 13:54:22 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 204 ms, Memory: 52,444 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.py
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.py
@@ -1,28 +1,26 @@
 
-# 가장 적은 비용으로 수영장을 이용할 수 있는 방법
-# 수영장 이용권 4종료
+# 수영장. 1년동안 각 달의 이용 계획 수립, 가장 적은 비용으로 수영장 이용할 수 있는 방법 찾기
+# 이용권 4종류 : 1일권, 1달권, 3달권, 1년권
 
-# 각 이용권의 요금과 각 달의 이용 계획이 주어질 때 가장 적은 비용으로 수영장을 이용할 수 있는 방법
-# -> DFS
+# 1년치 이용계획이 주어질 때, 가장 적은 비용으로 수영장 이용할 수 있는 방법 찾기.
 
-def DFS(L, money): # 현재 확인하고 있는 달, 현재까지 돈, 한달 이용권인지, 석달 이용권인지, 1년 이용권인지
+def DFS(L, sum_value):
     global res
-    if L >= 12:
-        res = min(res, money)
-    else:
-        if plan[L] > 0: # 처리해야 하는 경우
-            DFS(L+1, money + data[0] * plan[L]) # 1일권으로 현재 월을 처리
-            DFS(L+1, money + data[1]) # 1달권으로 현재 월을 처리
-            DFS(L+3, money + data[2]) # 3달권으로 처리
-        else: # 처리하지 않아도 되면 그냥 넘어가
-            DFS(L+1, money)
+    # print(f"{L+1}번달 까지의 상황 : {sum_value}")
+    if L >=12: # 모든 달 확인
+        res = min(res, sum_value)
+    else: # 현재 달을 어떻게 할 것인지
+        if plan[L] > 0:
+            DFS(L+1, sum_value + plan[L] * 이용권[0]) # 1일권으로 이번달 해결
+            DFS(L+1, sum_value + 이용권[1]) # 한달권으로 해겷
+            DFS(L+3, sum_value + 이용권[2]) # 3달권으로 해결
+        else: # 그냥 넘어가
+            DFS(L+1, sum_value)
 
 T = int(input())
 for t in range(1,T+1):
-    data = list(map(int, input().split())) # 이용권 가격 1일, 1달, 3달, 1년
-    plan = list(map(int, input().split())) # 12개월 이용계획
-    MAX = data[-1] # 1년 이용권을 썼을 때를 최대로 두고 계산
-
-    res = MAX # 1년권을 일단 최소 금액이라고 생각하고 진행
+    이용권 = list(map(int, input().split())) # 1일, 한달, 3달, 일년 이용권 정보
+    plan = list(map(int, input().split())) # 1월~12월까지 이용계획
+    res = 이용권[3] # 1년치를 최소값으로 보고 진행
     DFS(0,0)
     print(f"#{t} {res}")
