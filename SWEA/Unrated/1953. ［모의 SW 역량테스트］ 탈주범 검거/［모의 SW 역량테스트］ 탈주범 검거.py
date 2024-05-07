ffd27b31bcedc5c8841e656cb9139939f2cb990c
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2024-05-07 17:04:16 +0900

    [Unrated] Title: [모의 SW 역량테스트] 탈주범 검거, Time: 188 ms, Memory: 49,936 KB -BaekjoonHub

--- SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.py
+++ SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.py
@@ -1,65 +1,63 @@
 from collections import deque
 
 
-# 탈주범이 있을 수 있는 위치의 개수
-# 탈주범은 시간당 1의 거리 이동
-# 0초가 아닌 1초부터 진행
-# 가장 중요한 건, 이동할 좌표가 현재 위치와 파이프가 연결되어야 한다! -> 이 처리가 중요!!
+# 탈주범이 있을 수 있는 위치의 개수를 계산하기
+# 7개의 터널
+# 각 터널 별로 가능한 방향들을 따로 기록하고 사용하자
+# 각 칸의 값 중 1~7은 터널 구조물 번호, 숫자 0은 빈칸(터널이 없는 장소)
+# 1. 시작위치에서 탐색 진행
+# 2. 이동하고자 하는 위치와 현재 위치 모두 터널이어야 하고, 각 터널 구멍이 서로 연결되어야만 가능
+# 위 경우가 해당하면 큐에 넣고 해당 위치에서 재탐색 진행.
 
-# 각 파이프별로 이동 가능한 방향 처리 -> 리스트로 따로 선언
+터널 = [
+    [False, False, False, False],
+    [True, True, True, True],  # ! 1번은 모든 인덱스 가능
+    [True, False, True, False],  # !2번 터널은 상하
+    [False, True, False, True],  # ! 3번은 좌우
+    [True, True, False, False],  # ! 4번은 상우
+    [False, True, True, False],  # ! 5번은 하 우
+    [False, False, True, True],  # ! 6번은 하,좌 터널
+    [True, False, False, True]  # ! 7번은 상 좌
+]
 
 dx = [-1,0,1,0]
 dy = [0,1,0,-1]
-types = [
-    [0,0,0,0], # 0번 인덱스는 사용 안해
-    [1,1,1,1], # 1번 파이프는 모든 방향 이동 가능
-    [1,0,1,0], # 2번 파이프는 상 하
-    [0,1,0,1], # 3번 파이프는 좌우
-    [1,1,0,0], # 4번 파이프는 상 우
-    [0,1,1,0], # 5번 파이프는 하 우
-    [0,0,1,1], # 6번 파이프는 하 좌
-    [1,0,0,1] # 7번 파이프는 상 좌
-]
-
 def isInner(x,y):
     if 0<=x<n and 0<=y<m:
         return True
     return False
-
-def BFS(r,c,tp):
-    global visited
+def BFS(startX,startY,tunnel_num): # 시작위치, 터널번호 주어지면 탐색 진행
     dq = deque()
-    dq.append((r,c,tp)) # 시작 좌표, 해당 좌표의 파이프
-    visited[r][c] = True # 시작점 방문체크 필수 !!!!
-    time = 1 # 1초부터 시작
+    dq.append((startX,startY,tunnel_num))
+    visited = [[False] * m for _ in range(n)]
+    visited[startX][startY] = True # 시작위치 방문 처리
+
+    time = 1 # 시작 1부터
+    cnt = 1 # 시작위치 포함
     while dq:
         if time == L:
             break
-        size = len(dq)  #매 초마다 현재 들어있던 것들만 돌려야함
+        size = len(dq)
         for _ in range(size):
-            x,y,type = dq.popleft() # 현재 좌표, 현재 좌표의 파이프 번호
-            directions = types[type] # 해당 타입의 리스트 받아와서
+            x,y,num = dq.popleft() # 현재 위치, 현재 터널 번호
             for i in range(4):
-                if directions[i] == 1:
-                    nx = x + dx[i]
-                    ny = y + dy[i]
-                    if isInner(nx,ny) and not visited[nx][ny]: # 내부 좌표이면서 아직 방문 전
-                        if g[nx][ny] != 0 and types[g[nx][ny]][(i+2) % 4] == 1: # 이동할 좌표가 길이면서, 현재 좌표와 연결 (방향 반대로 연결)
-                            visited[nx][ny] = True
-                            dq.append((nx,ny,g[nx][ny]))
-        time += 1 # 다 끝나면 1초 추가
+                if not 터널[num][i]: continue # 해당 방향 이동 못하면 continue
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if not isInner(nx,ny): continue
+                if visited[nx][ny]: continue
+                # 이동하고자 하는 위치와 현재 위치 모두 터널, 연결 성 확인
+                if g[nx][ny] > 0 and 터널[g[nx][ny]][(i+2) %4]:
+                    dq.append((nx,ny,g[nx][ny]))
+                    visited[nx][ny] = True
+                    cnt += 1
+        time += 1
+    return cnt
 
 T = int(input())
-for t in range(1,T+1):
-    n,m,r,c,L = map(int, input().split()) # 터널 크기, 맨홀 뚜껑 시작 좌표, 탈출 후 소요 시간
+for t in range(1, T + 1):
+    n, m, x, y, L = map(int, input().split())  # 터널 쿠기, 시작 위치(x,y) , 탈출 후 L시간 후 가능한 위치
     g = [list(map(int, input().split())) for _ in range(n)]
-
-    visited = [[False] * m for _ in range(n)] # 가능한 방향 기록을 위해
-    BFS(r,c,g[r][c])
-    cnt = 0
-    for i in range(n):
-        for j in range(m):
-            if visited[i][j]:
-                cnt += 1
+    cnt = BFS(x,y,g[x][y])
 
     print(f"#{t} {cnt}")
