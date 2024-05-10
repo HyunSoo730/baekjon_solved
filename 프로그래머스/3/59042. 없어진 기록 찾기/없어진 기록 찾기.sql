3c5c573187d8a124fcdd0b9d0af282b26bf9b154
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2024-05-10 22:48:01 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,5 +1,17 @@
-SELECT B.ANIMAL_ID, B.NAME AS NAME
-FROM ANIMAL_OUTS B
-LEFT JOIN ANIMAL_INS A
+#동물 보호소에 들어온 동물의 정보 테이블 ANIMAL_INS
+# ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE
+# 동물 아이디, 생물 종,      보호 시작일,  보호 시작 시 상태,  이름,   성별 및 중성화 여부
+
+# 동물 보호소에서 입양 보낸 동물의 정보 담은 테이블 ANIMAL_OUTS
+# ANIMAL_ID, ANIMAL_TYPE, DATETIME, NAME, SEX_UPON_OUTCOME
+# 동물아이디, 생물 종,        입양일,    이름,   성별 및 중성화 여부
+
+# 입양 간 기록은 있는데, 보호소에 들어온 기록이 없는 동물의 ID와 이믈.  ID순으로 조회
+# -> 한쪽 데이터 없음 : LEFT JOIN을 통해 추출
+
+SELECT A.ANIMAL_ID, A.NAME
+FROM ANIMAL_OUTS A
+LEFT JOIN ANIMAL_INS B
 ON A.ANIMAL_ID = B.ANIMAL_ID
-WHERE B.ANIMAL_ID IS NOT NULL AND A.ANIMAL_ID IS NULL
+WHERE B.ANIMAL_ID IS NULL
+ORDER BY A.ANIMAL_ID ASC
