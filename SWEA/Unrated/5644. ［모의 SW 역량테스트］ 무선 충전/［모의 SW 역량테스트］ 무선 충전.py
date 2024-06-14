d0d74f4f7bd3f8698d0ce8a655a69cd986858539
조현수 <102610889+HyunSoo730@users.noreply.github.com>
2024-06-14 20:03:38 +0900

    [Unrated] Title: [모의 SW 역량테스트] 무선 충전, Time: 279 ms, Memory: 52,220 KB -BaekjoonHub

--- SWEA/Unrated/5644. ［모의 SW 역량테스트］ 무선 충전/［모의 SW 역량테스트］ 무선 충전.py
+++ SWEA/Unrated/5644. ［모의 SW 역량테스트］ 무선 충전/［모의 SW 역량테스트］ 무선 충전.py
@@ -1,91 +1,77 @@
 
+# 최적의 배터리 선택.
+# 충전범위 C 이하이면 해당 배터리 접속 가능.  (맨해튼 거리)
+# 배터리 2개 이상에 속하면 둘 중 하나를 선택해 접속 가능
+# 만약 한 배터리에 두 명의 사용자 접속 -> 접속한 사용자 수만큼 균등하게 분배
+# 사용자는 총 2명. 초기위치(0초)부터 충전 가능. 동시에 같은 위치 이동 가능
+# 시작부터 각 유저가 배터리에 속한지 확인해야함. 여러개 -> 경우의 수 모두 확인
+# A는 (1,1) B는 (10,10)에서 출발
 
 dx = [0,-1,0,1,0]
-dy = [0,0,1,0,-1]
-
-def step1(): # ! 각 유저별 속해있는 배터리 찾기
-    global userA, userB
-    global checkA, checkB
-    checkA = [False] * A
-    checkB = [False] * A
-    for i in range(A):
-        x,y,c,p = battery[i]
-        disA = abs(userA[0] - x) + abs(userA[1] - y)
-        disB = abs(userB[0] - x) + abs(userB[1] - y)
-        if disA <= c: # ! 해당 배터리에 속한다면
-            checkA[i] = True
-        if disB <= c:
-            checkB[i] = True
-    # ! 각 유저별 이용 가능한 배터리 계산 (이미 내림차순 정렬)
-
-def step2(): # ! 현재 시간에 A,B 배터리 이용 최대 효율 찾기
-    global userA, userB
-    global checkA, checkB
-    nowA, nowB = 0, 0  # ! 현재 A유저, B유저의 성능
-    maxA, maxB = 0, 0  # ! 둘의 최고 성능 배터리 값
-    indexA, indexB = -1, -1  # ! 현재 A유저, B유저의 이용하는 배터리 인덱스
-    res = 0  # ! 최고 합
-    for i in range(A):
-        nowA = battery[i][3]  # ! 현재 배터리 성능
-        nowB = battery[i][3]
-        if checkA[i]:  # ! A유저는 i번째 배터리 이용 가능
-            if maxA < nowA:
-                maxA = nowA
-                indexA = i  # ! 현재 인덱스가 최고 배터리로 갱신
-            if i == indexB:
-                res = max(res, (nowA + maxB) // 2)
-            else:
-                res = max(res, nowA + maxB)
-        if checkB[i]:
-            if maxB < nowB:
-                maxB = nowB
-                indexB = i
-            if i == indexA: # ! 현재 인덱스 비교 !!
-                res = max(res, (maxA + nowB) // 2)
-            else:
-                res = max(res, maxA + nowB)
+dy = [0,0,1,0,-1] # 이동X, 상, 우, 하, 좌
+def 최대충전량(userA,userB):
+    # 두 유저가 속하는 배터리를 받아와서 최대 충전량 찾기
+    res = 0
+    if userA and userB: # 둘 다 소속이 되어 있음
+        for i in userA:
+            for j in userB:
+                now = 배터리[i][3] + 배터리[j][3]
+                if i == j:
+                    now = now // 2
+                if res < now:
+                    res = now
+    elif userA: # A만 존재
+        for i in userA:
+            now = 배터리[i][3]
+            if res < now:
+                res = now
+    elif userB:
+        for i in userB:
+            now = 배터리[i][3]
+            if res < now:
+                res = now
     return res
 
-
-
-def simulate():
-    global checkA, checkB
-    global res
-
-    checkA = [False] * A
-    checkB = [False] * A
-    step1()
-    res += step2()
-    for time in range(M): # ! 0~M분까지 징행
-        directionA = moveA[time]
-        userA[0] += dx[directionA]
-        userA[1] += dy[directionA]
-
-        directionB = moveB[time]
-        userB[0] += dx[directionB]
-        userB[1] += dy[directionB]
-
-        checkA = [False] * A
-        checkB = [False] * A
-        step1()
-        now = step2()
-        res += now
+def 소속확인(x1,y1,x2,y2): # 두 유저의 현재 위치 가져와서 진행
+    # 유저 A,B 소속 확인
+    userA = []
+    userB = [] # 속하는 배터리 넣기 위해
+    for i in range(A): # 배터리 개수만큼 돌면서
+        tx,ty,c,power = 배터리[i] # 현재 배터리의 위치, 충전 범위, 충전량
+        diffA = abs(x1-tx) + abs(y1-ty)
+        if diffA <= c:
+            userA.append(i) # 속하는 충전 배터리 넘버
+        diffB = abs(x2-tx) + abs(y2-ty)
+        if diffB <= c:
+            userB.append(i)
+    res = 최대충전량(userA,userB)
+    return res
 
 
 T = int(input())
 for t in range(1,T+1):
-    M,A = map(int, input().split()) # ! 이동시간, 배터리 개수
-    moveA = list(map(int, input().split())) # ! 유저A의 움직임
-    moveB = list(map(int, input().split())) # ! 유저B의 움직임
-    battery = []
-    for i in range(A): # ! 배터리 정보
-        y,x,c,p = map(int, input().split())
-        battery.append((x,y,c,p)) # ! 배터리 정보 저장
-    battery.sort(key = lambda x : -x[3]) # ! 배터리 성능 순 내림차순
-    userA = [1,1]
-    userB = [10,10]
-    checkA = [False] * A
-    checkB = [False] * A
+    M,A = map(int, input().split()) # 총 이동 시간, 배터리 개수
+    userA = list(map(int, input().split())) # A 이동 방향
+    userB = list(map(int, input().split())) # B 이동 방향
+    배터리 = []
+    for _ in range(A):
+        x,y,c,power = map(int, input().split()) # 각 배터리의 위치, 충전 범위, 충전량
+        배터리.append((x,y,c,power))
+
+    x1,y1 = 1,1
+    x2,y2 = 10,10
+    now = 소속확인(x1,y1,x2,y2)
     res = 0
-    simulate()
+    res += now
+    for i in range(M):
+        dir = userA[i]
+        y1 += dx[dir]
+        x1 += dy[dir]
+        dir = userB[i]
+        y2 += dx[dir]
+        x2 += dy[dir]
+
+        now = 소속확인(x1,y1,x2,y2)
+        res += now
+    # 좌표 반대로 생각했어야 함 !!!
     print(f"#{t} {res}")
