0011361e85361e063b058ef0aed79573224699e8
조현수 <102610889+HyunSoo730@users.noreply.github.com>
2024-06-14 22:19:57 +0900

    [Unrated] Title: [모의 SW 역량테스트] 탈주범 검거, Time: 241 ms, Memory: 50,460 KB -BaekjoonHub

--- SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.py
+++ SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.py
@@ -1,63 +1,59 @@
 from collections import deque
 
-
-# 탈주범이 있을 수 있는 위치의 개수를 계산하기
-# 7개의 터널
-# 각 터널 별로 가능한 방향들을 따로 기록하고 사용하자
-# 각 칸의 값 중 1~7은 터널 구조물 번호, 숫자 0은 빈칸(터널이 없는 장소)
-# 1. 시작위치에서 탐색 진행
-# 2. 이동하고자 하는 위치와 현재 위치 모두 터널이어야 하고, 각 터널 구멍이 서로 연결되어야만 가능
-# 위 경우가 해당하면 큐에 넣고 해당 위치에서 재탐색 진행.
-
-터널 = [
-    [False, False, False, False],
-    [True, True, True, True],  # ! 1번은 모든 인덱스 가능
-    [True, False, True, False],  # !2번 터널은 상하
-    [False, True, False, True],  # ! 3번은 좌우
-    [True, True, False, False],  # ! 4번은 상우
-    [False, True, True, False],  # ! 5번은 하 우
-    [False, False, True, True],  # ! 6번은 하,좌 터널
-    [True, False, False, True]  # ! 7번은 상 좌
-]
-
+# 터널끼리 연결되어 있는 경우 이동 가능. 탈주범이 있을 수 있는 위치의 개수 구하기
+# 터널 종류 7종류.
+# 시간별로... BFS
 dx = [-1,0,1,0]
 dy = [0,1,0,-1]
 def isInner(x,y):
     if 0<=x<n and 0<=y<m:
         return True
     return False
-def BFS(startX,startY,tunnel_num): # 시작위치, 터널번호 주어지면 탐색 진행
-    dq = deque()
-    dq.append((startX,startY,tunnel_num))
-    visited = [[False] * m for _ in range(n)]
-    visited[startX][startY] = True # 시작위치 방문 처리
 
-    time = 1 # 시작 1부터
+def BFS(startX,startY,type):
+    dq = deque()
+    dq.append((startX,startY,type))
     cnt = 1 # 시작위치 포함
+    visited = [[False] * m for _ in range(n)]
+    visited[startX][startY] = True
+    time = 1
     while dq:
         if time == L:
             break
         size = len(dq)
         for _ in range(size):
-            x,y,num = dq.popleft() # 현재 위치, 현재 터널 번호
+            x,y,type = dq.popleft()
             for i in range(4):
-                if not 터널[num][i]: continue # 해당 방향 이동 못하면 continue
                 nx = x + dx[i]
                 ny = y + dy[i]
                 if not isInner(nx,ny): continue
                 if visited[nx][ny]: continue
-                # 이동하고자 하는 위치와 현재 위치 모두 터널, 연결 성 확인
-                if g[nx][ny] > 0 and 터널[g[nx][ny]][(i+2) %4]:
-                    dq.append((nx,ny,g[nx][ny]))
-                    visited[nx][ny] = True
-                    cnt += 1
+
+                if g[nx][ny] > 0: # 이동할 곳이 터널이면서
+                    if 터널[g[nx][ny]][(i+2)%4] == 1 and 터널[type][i] == 1: # 둘이 이어져있다면
+                        visited[nx][ny] = True
+                        dq.append((nx,ny,g[nx][ny]))
+                        cnt += 1 # 이동가능한 지역 추가
         time += 1
+
     return cnt
 
-T = int(input())
-for t in range(1, T + 1):
-    n, m, x, y, L = map(int, input().split())  # 터널 쿠기, 시작 위치(x,y) , 탈출 후 L시간 후 가능한 위치
-    g = [list(map(int, input().split())) for _ in range(n)]
-    cnt = BFS(x,y,g[x][y])
 
-    print(f"#{t} {cnt}")
+T = int(input())
+for t in range(1,T+1):
+    n,m,x,y,L = map(int, input().split())
+    g = [list(map(int, input().split())) for _ in range(n)]
+
+    터널 = [
+        [0,0,0,0], # 1번 인덱스부터 사용하기 위해
+        [1,1,1,1], # 1번 : 전부
+        [1,0,1,0], # 2번 : 상,하
+        [0,1,0,1], # 3번 : 좌,우
+        [1,1,0,0], # 4번 : 상,우
+        [0,1,1,0], # 5번 : 하,우
+        [0,0,1,1], # 6번 : 하,좌
+        [1,0,0,1]  # 7번 : 상,좌
+    ]
+
+    res = BFS(x,y,g[x][y]) # 시작위치, 시작위치의 터널 넘버
+    print(f"#{t} {res}")
