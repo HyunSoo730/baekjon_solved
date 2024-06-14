ecf284c7715b4b8ac5e08191654325563c4e6875
조현수 <102610889+HyunSoo730@users.noreply.github.com>
2024-06-14 19:08:17 +0900

    [Unrated] Title: [모의 SW 역량테스트] 점심 식사시간, Time: 450 ms, Memory: 58,132 KB -BaekjoonHub

--- SWEA/Unrated/2383. ［모의 SW 역량테스트］ 점심 식사시간/［모의 SW 역량테스트］ 점심 식사시간.py
+++ SWEA/Unrated/2383. ［모의 SW 역량테스트］ 점심 식사시간/［모의 SW 역량테스트］ 점심 식사시간.py
@@ -1,64 +1,74 @@
 import heapq
 from collections import deque
-# nxn 보드, 최대한 빠른 시간 내에 내려가기
-# 보드에 사람, 계단 입구 존재
-# 이동 완료 시간은 모든 사람들이 계단을 내려가 아래 층으로 이동을 완료하는 시간.
-# 게단 입구까지 이동시간 + 계단 내려가는 시간
 
-# 모든 사람들이 계단을 내려가 이동이 완료되는 시간이 최소가 되는 경우!
-# -> 완탐. 전부 다 해보기. 백트랙킹
 
-# 1 : 사람, 2이상 계단의 입구. 해당 값은 계단의 길이 의미
-# 계단의 입구는 반드시 2개, 서로 위치가 겹치지 않음
+# nxn 보드, 보드에는 사람, 계단 입구 존재
+# 이동 완료 시간 : 모든 사람들이 계단을 내려가 아래 층으로 이동을 완료한 시간
+# 아래층으로 이동하는 시간 : 계단입구까지 이동 시간 + 계단 내려가는 시간
+# 1. 계단 입구까지 시간 : 사람 위치 - 계단입구 위치
+# 2. 계단 내려가는 시간 : 1분 대기시간 + 계단은 최대 3명만 이용. 이미 3명 이용 시 그 중 한 명 계단 완전히 내려갈 때까지 대기
+# 구하고자 : 모든 사람들이 계단을 내려가 이동 완료하는 시간의 최소 시간
+
+# 1. 각 사람별 어느 계단으로 갈 지 선택 (DFS) (계단의 입구는 반드시 2개)
+# 2. 도착한 순서대로 계산 진행.
+
+def 이동시간계산(selected):
+    # 각 사람 별로 시간 계산
+    timeA = [] # 계단A를 사용하는 사람들의 시간 계산
+    timeB = []
+    for i in range(len(selected)): # 각 사람별 시간 계산
+        x,y = 사람[i] # i번째 사람의 위치
+        if selected[i] == 0: # i번쨰 사람이 첫번째 계단 선택
+            tx,ty,level = 계단[0] # 첫번째 계단의 위치, 해당 계단 내려가는 시간
+            time = abs(x-tx) + abs(y-ty) # 계단 입구까지의 시간
+            heapq.heappush(timeA, time)
+        else: # i번째 사람이 두번째 계단 선택
+            tx,ty,level = 계단[1] # 두번째 계단의 위치, 해당 계단 내려가는 시간
+            time = abs(x-tx) + abs(y-ty)
+            heapq.heappush(timeB, time)
+
+    resA = deque()
+    resB = deque()
+    while timeA: # 이제 도착 순서대로 삽입
+        time = heapq.heappop(timeA)
+        if len(resA) < 3:
+            resA.append(time)
+        else: # 비교
+            first = resA.popleft()
+            if first + 계단[0][2] < time: # 이후에 도착하면 그냥 삽입
+                resA.append(time)
+            else:
+                resA.append(first + 계단[0][2])
+
+    while timeB:
+        time = heapq.heappop(timeB)
+        if len(resB) < 3:
+            resB.append(time)
+        else:
+            first = resB.popleft()
+            if first + 계단[1][2] < time:
+                resB.append(time)
+            else:
+                resB.append(first + 계단[1][2])
+
+    res = 0
+    if resA:
+        res = max(res, resA[-1] + 계단[0][2] + 1)  #  1분은 대기시간
+    if resB:
+        res = max(res, resB[-1] + 계단[1][2] + 1)
+    return res
+
 
 def DFS(L):
-    global 최종결과
-    if L == len(사람): # 모두 어떤 계단갈 지 선택완료
-        # 각 계단 별로 확인
-        dataA = []
-        dataB = []
-        for idx in range(len(사람)):
-            if selected[idx] == 0: # idx번째 사람은 첫번째 계단을 선택한
-                diff = abs(사람[idx][0] - 계단[0][0]) + abs(사람[idx][1] - 계단[0][1]) # 계단까지 걸리는 시간
-                heapq.heappush(dataA,diff)
-            else:
-                diff = abs(사람[idx][0] - 계단[1][0]) + abs(사람[idx][1] - 계단[1][1])  # 계단까지 걸리는 시간
-                heapq.heappush(dataB, diff)
-        # 각 사람별 계단에 도착한 시간을 작은 순서로 덱에 저장
-        resA = deque()
-        resB = deque()
-        while dataA:  # 첫번째 계단.
-            도착시간 = heapq.heappop(dataA)
-            if len(resA) < 3: # 최대 인원 안넘으면 그냥 가능
-                resA.append(도착시간)
-            else: # 계단 오르는 인원이 3명이면 시간을 확인 후 진행
-                data = resA.popleft()
-                if data + 계단[0][2] < 도착시간: # 가장 먼저 계단을 이용한 사람의 종료시간과 새로 도착한 사람의 도착 시간 중 큰 값을 추가
-                    resA.append(도착시간)
-                else: # 대기시간 포함해서 넣기
-                    resA.append(data + 계단[0][2]) # 종료시간은 계단의 길이를 더하면 돼
-        while dataB: # 두번째 계단
-            도착시간 = heapq.heappop(dataB)
-            if len(resB) < 3:
-                resB.append(도착시간)
-            else: # 계단 오르는 인원이 3명이면 가장 먼저 계단을 이용한 사람의 종료시간과 현재 새로 온 사람의 시작 시간을 비교
-                data = resB.popleft()
-                if data + 계단[1][2] < 도착시간:
-                    resB.append(도착시간)
-                else:
-                    resB.append(data + 계단[1][2]) # 이 종료시간부터 새로온 사람이 계단을 이용할 수 이씅므로
-        # 두 계단 모두 확인 후에 각 계단 별로 마지막 사람의 종료시간을 계산하고, 그 중 최댓값 계산 (대기시간 1분 추가로 넣는 거 잊지말기)
-        rA = rB = 0
-        if len(resA) > 0:
-            rA = resA.pop() + 계단[0][2] + 1 # 가장 늦게 계단 도착한 사람의 도착 시간 + 계단 길이 + 1(대기시간)
-        if len(resB) > 0:
-            rB = resB.pop() + 계단[1][2] + 1
-        result = max(rA,rB) # 최종결과는 두 계단에서 가장 마지막에 내려온 사람의 최종 시간.
-        최종결과 = min(최종결과, result)
-    else:
-        selected[L] = 0 # L번째 사람 첫번째 계단 선택
+    global min_time
+    if L == len(사람): # 모든 사람 선택 완료
+        # 모든 사람이 어떤 계단으로 갈 지 선택했으니 이제 각 계단 별로 확인 진행
+        time = 이동시간계산(selected)
+        min_time = min(min_time, time)
+    else: # 각 사람 이제 계단 선택해야 함.
+        selected[L] = 0 # L번째 사람 첫번째 계단으로 가겠다.
         DFS(L+1)
-        selected[L] = 1 # L번째 사람 두번째 계단 선택
+        selected[L] = 1 # L번째 사람 두번째 계단으로 가겠다.
         DFS(L+1)
 
 
@@ -66,17 +76,18 @@ T = int(input())
 for t in range(1,T+1):
     n = int(input())
     g = [list(map(int, input().split())) for _ in range(n)]
-    계단 = [] # 계단 입구는 2개
+
+    계단 = []
     사람 = []
     for i in range(n):
         for j in range(n):
-            if g[i][j] == 1:
-                사람.append((i,j))
-            if g[i][j] >= 2: # 계단 입구
-                계단.append((i,j,g[i][j])) # 계단 좌표, 계단 길이
-    # 각 계단은 동시에 최대 3명만 올라가 있을 수 있따.
-    # 1. 각 사람들이 어떤 계단을 갈 지 선택
+            if g[i][j] >= 2: # 계단
+                계단.append((i,j,g[i][j])) # 계단 위치 추가
+            elif g[i][j] == 1: # 사람
+                사람.append((i,j)) # 사람 위치 추가
+    # step1. 각 사람 어느 계단 선택할 지 선택.
     selected = [0] * len(사람)
-    최종결과 = int(1e9)
+    min_time = int(1e9)
     DFS(0)
-    print(f"#{t} {최종결과}")
+
+    print(f"#{t} {min_time}")
