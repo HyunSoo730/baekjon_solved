3d4e71d84dc1036d032591ce3da99736b9d25a51
조현수 <102610889+HyunSoo730@users.noreply.github.com>
2024-06-27 15:52:16 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/README.md
+++ 프로그래머스/3/42627. 디스크 컨트롤러/README.md
@@ -1,10 +1,10 @@
 # [level 3] 디스크 컨트롤러 - 42627 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.04 ms
+메모리: 10.1 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 27일 14:29:42
+2024년 06월 27일 15:52:14
 
 ### 문제 설명
 
--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -3,21 +3,23 @@ import heapq
 def solution(jobs):
     n = len(jobs)
     jobs.sort(key=lambda x: x[0])  # 요청 시간을 기준으로 오름차순 정렬
-    
-    current_time = 0
-    total_time = 0
-    waiting_queue = []
-    
-    while jobs or waiting_queue:
-        while jobs and jobs[0][0] <= current_time:
-            request_time, processing_time = jobs.pop(0)
-            heapq.heappush(waiting_queue, (processing_time, request_time))
-        
-        if waiting_queue:
-            processing_time, request_time = heapq.heappop(waiting_queue)
-            current_time += processing_time
-            total_time += current_time - request_time
+    temp = []
+    last = 0  # 가장 마지막 작업의 끝나는 시간
+    res = 0
+    cur_time = 0  # 현재 시간
+
+    while jobs or temp:
+        while jobs and jobs[0][0] <= cur_time:
+            start, time = jobs.pop(0)
+            heapq.heappush(temp, (time, start))  # 처리 시간, 요청 시각을 튜플로 힙에 삽입
+
+        if temp:
+            time, start = heapq.heappop(temp)
+            last = cur_time
+            cur_time = max(cur_time + time, start + time)
+            res += cur_time - start
         else:
-            current_time = jobs[0][0]
-    
-    return total_time // n
+            cur_time = jobs[0][0]
+
+    res //= n
+    return res
