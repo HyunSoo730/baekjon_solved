f30b7f5e8628d6bb27cdc021ee9897c66745f303
조현수 <102610889+HyunSoo730@users.noreply.github.com>
2024-07-05 16:36:40 +0900

    [level 2] Title: 재구매가 일어난 상품과 회원 리스트 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
+++ 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -1,7 +1,11 @@
-# 온라인 상품 판매 정보 테이블 ONLINE_SALE
-# 재구매 -> 개수가 2개 이상.
+# 쇼핑몰의 온라인 상품 판매 정ㅈ보 담은 ONLINE_SALE 테이블
+
+# 동일한 회원이 동일한 상품을 재구매한 데이터를 구하여
+# 재구매한 회원 ID와 재구매한 상품 ID 출력하기
+# 회원 ID 기준 오름차순 정렬, 회원 ID 같다면 상품 ID 기준 내림차순 정렬 
+
 SELECT USER_ID, PRODUCT_ID
-FROM ONLINE_SALE
-GROUP BY PRODUCT_ID, USER_ID
-HAVING COUNT(PRODUCT_ID) >= 2
-ORDER BY USER_ID ASC, PRODUCT_ID DESC
+FROM ONLINE_SALE 
+GROUP BY USER_ID, PRODUCT_ID
+HAVING COUNT(*) >= 2
+ORDER BY USER_ID, PRODUCT_ID DESC
