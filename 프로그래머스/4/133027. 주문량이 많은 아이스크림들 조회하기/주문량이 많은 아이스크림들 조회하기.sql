055f6684c8412605e0bb4555c9226d19cd5b4fba
조현수 <102610889+HyunSoo730@users.noreply.github.com>
2024-07-08 16:38:52 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,12 +1,23 @@
-# 아이스크림 가게 상반기 주문정보 담은 FIRST_HALF 테이블
-# SHIPMENT_ID(출하번호), FLAVOR(아이스크림 맛), TOTAL_ORDER(상반기 아이스크림 총무준량)
-# 7월의 아이스크림 주문 정보 담은 JULY 테이블
-# SHIPMENT_ID(출하번호), FLAVOR(아이스크림 맛), TOTAL_ORDER(7월 아이스크림 총 주문량)
+# 아이스크림 가게 상반기 주문 정보 FIRST_HALF 테이블
+# 7월의 아이스크림 주문 정보 JULY 테이블
 
-# 7월 아이스크림 총 주문량과 상반기 아이스크림 총 주문량을 더한 값이 큰 순서대로 상위 3개의 맛 조회
-SELECT F.FLAVOR
-FROM FIRST_HALF F
-JOIN JULY J ON F.FLAVOR = J.FLAVOR
-GROUP BY F.FLAVOR
-ORDER BY SUM(F.TOTAL_ORDER) + SUM(J.TOTAL_ORDER) DESC
-LIMIT 3;
+# 7월 아이스크림 총 주문량과 상반기 아이스크림 총 주문량을 더한 값이 큰 순서대로
+# 상위 3개의 맛을 조회하는 sql문 작성
+
+WITH FIRST_HALF_SUM AS (
+    SELECT FLAVOR, SUM(TOTAL_ORDER) AS SUM_A
+    FROM FIRST_HALF
+    GROUP BY FLAVOR
+),
+JULY_SUM AS (
+    SELECT FLAVOR, SUM(TOTAL_ORDER) AS SUM_B
+    FROM JULY
+    GROUP BY FLAVOR
+)
+
+SELECT A.FLAVOR
+FROM FIRST_HALF_SUM A
+JOIN JULY_SUM B
+ON A.FLAVOR = B.FLAVOR
+ORDER BY SUM_A + SUM_B DESC
+LIMIT 0,3
