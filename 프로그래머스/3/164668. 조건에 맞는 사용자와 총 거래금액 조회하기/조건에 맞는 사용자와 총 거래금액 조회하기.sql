32510513f9eca3f59e352aaef9b3cae08d2db8d4
조현수 <102610889+HyunSoo730@users.noreply.github.com>
2024-07-09 10:14:40 +0900

    [level 3] Title: 조건에 맞는 사용자와 총 거래금액 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
+++ 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
@@ -1,20 +1,17 @@
-# 중고 거래 게시판 정보 USED_GOODS_BOARD 테이블
-# BOARD_ID, WRITER_ID, TITLE, CONTENTS, PRICE, CREATED_DATE, STATUS, VIEWS
-# 게시글 ID,   작성자 ID, 게시글 제목, 게시글 내용, 가격, 작성일,      거래 상태, 조회수
+# 중고거래 게시판 정보 USED_GOODS_BOARD 테이블
+# 중고거래 게시판 사용자 정보 USED_GOODS_USER 테이블
 
-# 중고 거래 게시판 사용자 정보 USED_GOODS_USER 테이블
-# USER_ID, NICKNAME, CITY, STREET_ADDRESS1, STREET_ADDRESS2, TLNO
-# 회원 ID, 닉네임,    시 ,     도로명 주소,     상세 주소,        전화 번호
-
-# 완료된 중고 거래의 총 금액이 70만원 이상인 사람의 -> STATUS가 DONE이면 거래 완료.
+# 두 테이블에서 완료된 중고거래의 -> 거래 상태 확인
+# 총금액이 70만원 이상인 사람의 -> GROUP BY 하고 SUM
 # 회원ID, 닉네임, 총거래금액 조회
-# 총거래금액 기준 오름차순 정렬
+# 총거래금액 오름차순
 
-SELECT B.USER_ID, B.NICKNAME, SUM(PRICE) AS TOTAL_SALES
+SELECT B.USER_ID, B.NICKNAME, SUM(A.PRICE) AS TOTAL_SALES
 FROM USED_GOODS_BOARD A
 JOIN USED_GOODS_USER B
 ON A.WRITER_ID = B.USER_ID
 WHERE A.STATUS = "DONE"
-GROUP BY B.USER_ID, B.NICKNAME
-HAVING TOTAL_SALES >= 700000
+GROUP BY B.USER_ID
+HAVING SUM(A.PRICE) >= 700000
 ORDER BY TOTAL_SALES ASC
+
