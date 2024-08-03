546f8a0cf02c8a8fc2b9bcecbcd5013682bb47ad
조현수 <102610889+HyunSoo730@users.noreply.github.com>
2024-08-03 19:30:30 +0900

    [Gold IV] Title: 치즈, Time: 160 ms, Memory: 114280 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.py
+++ 백준/Gold/2636. 치즈/치즈.py
@@ -1,56 +1,64 @@
 import sys
 from collections import deque
 
-# 가장자리에는 치즈가 놓여있지 않으며, 치즈에는 하나 이상의 구멍이 있을 수 있다.
-# 가장 자리 따라가면서 녹아야 함.
-# 좌표 확장 개념 도입
+# 공기와 접촉된 칸은 한 시간이 지나면 녹아 없어짐
+# 공기의 관점에서 생각의 전환 !!
+# 공기 중에서 치즈가 모두 녹아 없어지는 데 걸리는 시간,
+# 모두 녹기 한 시간 전에 남아있는 치즈조각이 놓여 있는 칸의 개수
+
 
 n,m = map(int, input().split())
 g = [list(map(int, input().split())) for _ in range(n)]
 
-# 공기와 접촉된 칸은 한 시간이 지나면 녹아 없어진다.
-# -> 중요한 것은 반대로 생각할 수 있어야 함. 공기에서 탐색을 시작한다는 생각!
-startX,startY = 0,0
-
+# 0은 공기, 1은 치즈
+air = 0
+cheeze = 1
 dx = [-1,0,1,0]
 dy = [0,1,0,-1]
 def isInner(x,y):
-    return 0<=x<n and 0<=y<m
+    if 0<=x<n and 0<=y<m:
+        return True
+    return False
 
 def BFS(startX,startY):
     dq = deque()
     dq.append((startX,startY))
-    melt = deque()
     visited = [[False] * m for _ in range(n)]
     visited[startX][startY] = True
-    cnt = 0 # 녹인 치즈
+    cnt = 0 # 치즈 개수
+    melt = deque()
     while dq:
         x,y = dq.popleft()
         for i in range(4):
             nx = x + dx[i]
-            ny = y +dy [i]
+            ny = y + dy[i]
             if not isInner(nx,ny): continue
-            if not visited[nx][ny] and g[nx][ny] == 0:
+            if visited[nx][ny]: continue
+            if g[nx][ny] == air:
                 visited[nx][ny] = True
                 dq.append((nx,ny))
-            elif not visited[nx][ny] and g[nx][ny] == 1:
+            elif g[nx][ny] == cheeze:
                 visited[nx][ny] = True
                 melt.append((nx,ny))
+                cnt += 1
 
     while melt:
         x,y = melt.popleft()
-        g[x][y] = 0
-        cnt += 1
+        g[x][y] = air # 공기화
     return cnt
 
+
+
+
+last_cnt = 0
 time = 0
-prev_cnt = 0
 while True:
-    cnt = BFS(0, 0)
-    if cnt == 0:
-        break
-    prev_cnt = cnt
+
+    cnt = BFS(0,0)
+    if cnt == 0: # 끝
+         break
     time += 1
+    last_cnt = cnt
 
 print(time)
-print(prev_cnt)
+print(last_cnt)
