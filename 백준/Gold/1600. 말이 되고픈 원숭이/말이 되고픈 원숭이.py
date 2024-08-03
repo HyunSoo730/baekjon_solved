f8b273856de044b5bb851918a1be17b964f709ed
조현수 <102610889+HyunSoo730@users.noreply.github.com>
2024-08-03 12:56:24 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 548 ms, Memory: 157080 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.py
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.py
@@ -1,68 +1,74 @@
 import sys
+
 from collections import deque
 
+# 말 움직임 8가지, 말은 장애물 뛰어넘을 수 있음
+# 원숭이는 상하좌우만 가능, 또한 말처럼 총 k번만 이동 가능
+# -> 상태가 있음 -> 상태가 있다는 것은 3차원..
 
-# 말 : 이동방향 8가지, 말로 움직이면 장애물 뛰어넘을 수 있다.
-# 원숭이는 k번만 말처럼 이동 가능 그 외는 4방 탐색.
-# (0,0) -> (h-1,w-1)  최소횟수로 이동 -> 최단거리 : 다익스트라
-# 보드의 각 칸, 0은 이동가능 1은 장애물(벽)
-
-k = int(input())  # 말처럼 이동가능 횟수
-m,n = map(int, input().split())  # 보드 크기
+k = int(input())
+m, n = map(int, input().split())  # 가로, 세로
 g = [list(map(int, input().split())) for _ in range(n)]
 
-INF = int(1e9)
-dis = [[[INF] * (k + 1) for _ in range(m)] for _ in range(n)]  # 최단거리 기록
-# k+1로 한 것은 k번까지 가능하기 때문
+# 0인 빈칸, 1은 벽, 벽이 있는 곳으로 이동 불가
+blank = 0
+wall = 1
+
+# (0,0) -> (n-1,m-1) 로 이동해야함 -> 최단거리
+
+startX, startY = 0, 0
+endX, endY = n - 1, m - 1
 
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
+hx = [-2, -2, -1, 1, 2, 2, 1, -1]
+hy = [-1, 1, 2, 2, 1, -1, -2, -2]
 
-hx = [-2, -2, -1, -1, 1, 1, 2, 2]
-hy = [-1, 1, -2, 2, -2, 2, -1, 1] # 말처럼 이동
+
 def isInner(x, y):
     if 0 <= x < n and 0 <= y < m:
         return True
     return False
 
-startX,startY = 0,0
-endX,endY = n-1,m-1 # 도착점
 
-def BFS(a, b):
+def BFS(startX, startY):
     global res
     dq = deque()
-    dq.append((a, b, 0, 0))  # 시작좌표, 현재 경로의 거리, 말처럼 이동한 횟수 기억
+    dq.append((startX, startY, 0, 0))  # 시작점, 이동거리, 말로 움직인 횟수
+    INF = int(1e9)
+    # [n][m][k]
+    dis = [[[INF] * (k+1) for _ in range(m)] for _ in range(n)]  # [n][m][k] 만듬
+    dis[startX][startY][0] = 0  # 시작점 초기화
 
     while dq:
-        x, y, t, cnt = dq.popleft()  # 현재 위치, 현재 경로의 길이, 말처럼 이동한 횟수
-        if (x,y) == (endX, endY): # 도착점 도달
-            res = min(res, t) # 최단거리 갱신
-            continue # 도착점 도달하고는 더이상 하면 안됨.
-        # 현재 위치에서 기본 이동, 말처럼이동 다 따져보기
+        x, y, d, cnt = dq.popleft()
+        if (x,y) == (endX,endY):
+            res = min(res,d)
+            break
+        # 먼저 원숭이 움직임
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if not isInner(nx, ny):  # 내부 좌표 아니면 안돼
-                continue
-            if g[nx][ny] == 0:  # 이동가능
-                if t + 1 < dis[nx][ny][cnt]:  # 현재 말로 이동한 횟수에서의 최단거리보다 작다면
-                    dis[nx][ny][cnt] = t + 1
-                    dq.append((nx, ny, t + 1, cnt))
-        for i in range(8):
-            nx = x + hx[i]
-            ny = y + hy[i]
-            if not isInner(nx,ny):
-                continue # 내부 좌표 아니면 다시
-            if g[nx][ny] == 0: # 말처럼 뛰었을 때 이동가능. 근데 횟수가 남아있는지 확인
-                if cnt < k: # 말처럼 이동 가능
-                    if t + 1 < dis[nx][ny][cnt + 1]: # 말처럼 이동 + 1의 보드에서 최단거리 갱신 가능
-                        dis[nx][ny][cnt + 1] = t + 1
-                        dq.append((nx,ny,t + 1 , cnt + 1))
+            if not isInner(nx, ny): continue
+            if g[nx][ny] == blank:  # 이동 가능
+                if d + 1 < dis[nx][ny][cnt]:  # 갱신 가능
+                    dis[nx][ny][cnt] = d + 1
+                    dq.append((nx, ny, d + 1, cnt))
+        # 말 움직임
+        if cnt < k:  # 아직 말로 이동 가능
+            for i in range(8):
+                nx = x + hx[i]
+                ny = y + hy[i]
+                if not isInner(nx, ny): continue
+                if g[nx][ny] == blank:
+                    if d + 1 < dis[nx][ny][cnt + 1]:
+                        dis[nx][ny][cnt + 1] = d + 1
+                        dq.append((nx,ny,d+1,cnt+1))
 
 
 res = int(1e9)
-BFS(startX, startY)
-if res == INF:
+BFS(startX,startY)
+if res == int(1e9):
     print(-1)
 else:
     print(res)
