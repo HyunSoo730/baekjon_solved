4c84218d93e5237a875491d78fa800edd223d33d
조현수 <102610889+HyunSoo730@users.noreply.github.com>
2024-08-04 12:56:24 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,40 +1,30 @@
-# 고객 정보 MEMBER_PROFILE 테이블
-# MEMBER_ID, MEMBER_NAME, TLNO, GENDER, DATE_OF_BIRTH
-# 회원ID,    회원 이름,    회원 연락처, 성별,  생년월일
+# 고객 정보를 담은 MEMBER_PROFILE 테이블
+# 식당 리뷰 정보를 담은 REST_REVIEW 테이블
 
-# 리뷰 정보 REST_VIEW 테이블
-# REVIEW_ID, REST_ID, MEMBER_ID, REVIEW_SCORE, REVIEW_TEXT, REVIEW_DATE
-# 리뷰ID,    식당ID,  회원 ID,     점수,          리뷰 텍스트,     리뷰 작성일
+# 두 테이블에서 리뷰를 가장 많이 작성한 회원의 리뷰들을 조회
+# 회원 이름, 리뷰 텍스트, 리뷰 작성일 출력
+# 리뷰 작성일(REVIEW_DATE) 기준 오름차순, 리뷰 텍스트(REVIEW_TEXT) 기준 오름차순
 
-# 리뷰를 가장 많이 작성한 회원의 리뷰들을 조회하는 SQL
-# 회원 이름, 리뷰 텍스트, 리뷰 작성일 출력. 리뷰 작성일 오름차순, 리뷰 텍스트 오름차순
-
-# 1. 리뷰 가장 많이 작성한 회원 찾기
-WITH MAX_REVIEWER AS (
-    SELECT MEMBER_ID, COUNT(*) AS REVIEW_COUNT
-    FROM REST_REVIEW
-    GROUP BY MEMBER_ID
-    ORDER BY REVIEW_COUNT DESC
+WITH TEMP AS (
+    SELECT A.MEMBER_ID
+    FROM MEMBER_PROFILE A
+    JOIN REST_REVIEW B
+    ON A.MEMBER_ID = B.MEMBER_ID 
+    GROUP BY A.MEMBER_ID 
+    ORDER BY COUNT(*) DESC
     LIMIT 1
+),
+TT AS (
+    SELECT A.MEMBER_ID, A.MEMBER_NAME, B.REVIEW_TEXT, B.REVIEW_DATE
+    FROM MEMBER_PROFILE A
+    JOIN REST_REVIEW B
+    ON A.MEMBER_ID = B.MEMBER_ID
 )
-SELECT 
-    MP.MEMBER_NAME, 
-    RR.REVIEW_TEXT, 
-    DATE_FORMAT(RR.REVIEW_DATE, "%Y-%m-%d")
-FROM 
-    MAX_REVIEWER MR
-    JOIN REST_REVIEW RR ON MR.MEMBER_ID = RR.MEMBER_ID
-    JOIN MEMBER_PROFILE MP ON RR.MEMBER_ID = MP.MEMBER_ID
-ORDER BY 
-    RR.REVIEW_DATE ASC, 
-    RR.REVIEW_TEXT ASC;
 
-
-
-
-
-
-
-
-
+SELECT MEMBER_NAME, REVIEW_TEXT, DATE_FORMAT(REVIEW_DATE, "%Y-%m-%d") AS REVIEW_DATE
+FROM TT
+WHERE MEMBER_ID = (
+    SELECT MEMBER_ID FROM TEMP
+)
+ORDER BY REVIEW_DATE ASC, REVIEW_TEXT ASC
 
