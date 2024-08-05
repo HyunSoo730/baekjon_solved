f8ffebd4b4f007408a7bea51ba807203ee522fd1
조현수 <102610889+HyunSoo730@users.noreply.github.com>
2024-08-05 13:40:46 +0900

    [Unrated] Title: [모의 SW 역량테스트] 디저트 카페, Time: 3,984 ms, Memory: 87,032 KB -BaekjoonHub

--- SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.py
+++ SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.py
@@ -1,43 +1,51 @@
 
-# 디저트 카페, nxn 보드, 각 셀의 값은 디저트의 종류를 의미.
-# 대각선 방향으로 움직일 수 있음
-# 특정 칸에서 출발하여 대각선 방향으로 움직이고 사각형 모양을 그리며 출발위치로 돌아와야 함.
-# 중복 안됨. 꼭 사각형으로 완성해서 와야함.
-# 디저트를 가장 많이 먹을 수 있는 경로 찾기.
-# 모든 위치에서 시작 가능. 방향은 고정적으로 가져가도 됨
-# 현재 방향으로 가거나, 꺾거나 둘 중 하나.
+# nxn 격자
+# 각 칸의 값은 디저트 종류를 의미
+# 특정 출발점에서 출발해서 대각선 방향으로 움직이고 사각형 모양을 그리며 출발한 카페로 돌아와야 한다.
+# 같은 종류 (값이 같은) 먹으면 안됨
+# 디저트 많이 먹기
 
-dx = [1,1,-1,-1]
-dy = [-1,1,1,-1]
+dx = [-1, 1, 1, -1]
+dy = [1, 1, -1, -1]
+
 def isInner(x,y):
     if 0<=x<n and 0<=y<n:
         return True
     return False
 
-def DFS(L, x,y,dir,now,startX,startY, visited): # 방향전환 횟수,현재 위치, 현재 방향, 현재까지의 개수, 방문 종류
+# 시작은 무조건 0번 방향에서 진행. 3번 바뀌고 기존 위치로 돌아오면 끝
+def DFS(startX,startY,x,y,dir,change, visited):
     global res
-    if L > 3: return # 방향 바꾼 횟수가 3번 넘어가면 안된다.
-    if L == 3 and (x,y) == (startX,startY):
-        res = max(res, now)
-    # 현재 방향으로 그대로 가거나, 방향을 꺾거나 2가지로 진행
-    else:
+    if change > 3: # 가지치기
+        return
+    if (x,y) == (startX,startY) and change == 3:
+        res = max(res, len(visited))
+    else: # 현재 위치에서 기존 방향으로 계속 가냐 or 방향 바꾸냐 두가지
         for i in range(2):
-            direction = (dir + i) % 4
-            nx = x + dx[direction]
-            ny = y + dy[direction]
-            if isInner(nx,ny) and g[nx][ny] not in visited:
-                visited.append(g[nx][ny])
-                DFS(L+i, nx,ny,direction, now+1, startX,startY, visited)
-                visited.pop()
+            dir = (dir + i) % 4
+            nx = x + dx[dir]
+            ny = y + dy[dir]
+            if not isInner(nx,ny): continue
+            if g[nx][ny] in visited: # 이미 존재
+                continue
+            visited.append(g[nx][ny]) # 추가 후
+            DFS(startX,startY,nx,ny,dir,change + i, visited)
+            visited.pop()
+
 
 
 T = int(input())
-for t in range(1,T+1):
+for t in range(1, T + 1):
     n = int(input())
     g = [list(map(int, input().split())) for _ in range(n)]
 
-    res = -1
-    for x in range(n):
-        for y in range(n): # 모든 위치에서 판단 진행
-            DFS(0,x,y,0,0,x,y, []) # 매 경로마다 확인해야 하므로 파라미터로 존재해야함
-    print(f"#{t} {res}")
+    res = 0
+    for i in range(n):
+        for j in range(n):
+            visited = []
+            DFS(i,j,i,j,0,0,visited)
+
+    if res == 0:
+        print(f"#{t} {-1}")
+    else:
+        print(f"#{t} {res}")
