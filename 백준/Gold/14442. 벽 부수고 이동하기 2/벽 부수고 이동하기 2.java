12eb275a782a54afd2f260f511d9848e3fa8bb7d
조현수 <102610889+HyunSoo730@users.noreply.github.com>
2024-09-17 16:09:05 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1508 ms, Memory: 370160 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -1,22 +1,27 @@
-import javax.xml.crypto.Data;
-import java.awt.*;
+import java.io.IOException;
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
+
+    // ! N*M 보드
+    // ! 맵에서 0은 이동 가능, 1은 벽
+    // ! (1,1) -> (N,M) 이동. 최단경로
+    // ! 벽 부수고 최대 K개까지 부수고 이동 가능
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
 
-    static int n,m,k;  // ! k는 벽을 부술 수 있는 최대 횟수
+    static int n,m,k; // ! k는 벽을 부술 수 있는 횟수
     static int[][] g;
     static int[][][] dis;
-    static int endX,endY;
-    static int INF = 1000000, min_dis;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int endX,endY, INF = 10000000;
+    static int MIN_DIS = INF;
 
-    private static class Data{
-        int x, y, dis, cnt;
+    private static class Data {
+        int x,y,dis,cnt;
 
         Data(int x, int y, int dis, int cnt) {
             this.x = x;
@@ -25,9 +30,6 @@ public class Main {
             this.cnt = cnt;
         }
     }
-
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
 
     public static boolean isInner(int x, int y) {
         return x >= 0 && x < n && y >= 0 && y < m;
@@ -39,61 +41,68 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
-        g = new int[n][m];
-        dis = new int[n][m][k+1];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                Arrays.fill(dis[i][j], INF);
-            }
-        }
 
+        g = new int[n][m];
         for (int i = 0; i < n; i++) {
             String line = br.readLine();
             for (int j = 0; j < m; j++) {
                 g[i][j] = line.charAt(j) - '0';
             }
         }
-        endX = n-1; endY = m-1;
-        min_dis = INF;
+
+        // ! 최단거리 기록하는 dis에 최댓값으로 채워넣기
+        dis = new int[n][m][k+1]; // ! 최대 k번
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                Arrays.fill(dis[i][j], INF);
+            }
+        }
+        endX = n-1;
+        endY = m-1;
+
         BFS(0, 0);
-        if (min_dis == INF) {
+        if (MIN_DIS == INF) {
             System.out.println(-1);
         } else {
-            System.out.println(min_dis);
+            System.out.println(MIN_DIS);
         }
-
     }
 
     public static void BFS(int startX, int startY) {
         Deque<Data> dq = new ArrayDeque<>();
-        dq.offer(new Data(startX, startY, 1, 0));
+        dq.offer(new Data(startX, startY, 1, 0)); // ! 시작지점 포함
+        dis[startX][startY][0] = 0; // ! 시작지점 세팅
 
         while (!dq.isEmpty()) {
-            Data now = dq.poll();
+            Data now = dq.poll(); // ! 맨 앞에 꺼내서 확인
             if (now.x == endX && now.y == endY) {
-                min_dis = now.dis;
-                break;
+                MIN_DIS = Math.min(MIN_DIS, now.dis);
             }
             for (int i = 0; i < 4; i++) {
                 int nx = now.x + dx[i];
                 int ny = now.y + dy[i];
-                if(!isInner(nx,ny)) continue;
+                // ! 이동이 가능하면서, 최단거리일 때 진행
+                if (!isInner(nx, ny))
+                    continue;
 
-                if (g[nx][ny] == 0) { // ! 이동 가능
-                    if (now.dis + 1 < dis[nx][ny][now.cnt]) {
-                        dis[nx][ny][now.cnt] = now.dis + 1; // ! 갱신
-                        dq.offer(new Data(nx, ny, now.dis + 1, now.cnt)); // ! 갱신 됐으니 다시 해당 지점에서 시작
+                if (g[nx][ny] == 0) { // ! 이동 가능한 위치
+                    if (now.dis + 1 < dis[nx][ny][now.cnt]) { // ! 최단거리 갱신 가능
+                        dis[nx][ny][now.cnt] = now.dis + 1;
+                        dq.offer(new Data(nx, ny, now.dis + 1, now.cnt));
                     }
-                } else if (g[nx][ny] == 1) { // ! 벽인 경우
-                    if (now.cnt < k) { // ! 벽을 더 깰 수 있다면
+                }else{ // ! 이동 못하더라도 벽 부수고 갈 수 있는지 확인
+                    if (now.cnt < k) {
                         if (now.dis + 1 < dis[nx][ny][now.cnt + 1]) {
-                            dis[nx][ny][now.cnt + 1] = now.dis + 1;
+                            dis[nx][ny][now.cnt+1] = now.dis + 1;
                             dq.offer(new Data(nx, ny, now.dis + 1, now.cnt + 1));
                         }
                     }
                 }
             }
         }
+
+
+
     }
 
 }
