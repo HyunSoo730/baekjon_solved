765b9b567b435bd06bbaa3736dae742084e4ac53
조현수 <102610889+HyunSoo730@users.noreply.github.com>
2025-08-09 16:58:57 +0900

    Time: 499 ms (88.55%), Space: 0B (100%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -1,24 +1,20 @@
-# 조건1,2의 반대로 해서 진행
+# tiv_2016 총합 구하기
+# 조건1. tiv_2015가 같은 사람이 있는
+# 조건2. (lat, lon)이 중복되지 않는
+
+# 조건 충족 A and B
 WITH tempA as (
     select tiv_2015
     from Insurance
     group by tiv_2015
-    having count(*) <= 1
+    having count(*) > 1
 ), tempB as (
     select lat, lon
     from Insurance
     group by lat, lon
-    having count(*) > 1
-), tempC as (
-    select * from Insurance
+    having count(*) = 1
 )
 select round(sum(tiv_2016), 2) as tiv_2016
-from Insurance i
-where NOT EXISTS (
-    select 1 from tempA a
-    where a.tiv_2015 = i.tiv_2015
-)
-and NOT EXISTS (
-    select 1 from tempB b
-    where b.lat = i.lat and b.lon = i.lon
-)
+from Insurance
+where tiv_2015 in (select tiv_2015 from tempA) 
+    and (lat, lon) in (select lat, lon from tempB)
