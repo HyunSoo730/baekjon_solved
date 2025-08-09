bbe146b0b8b5edef82f31160a25903448bf044f7
조현수 <102610889+HyunSoo730@users.noreply.github.com>
2025-08-09 17:33:58 +0900

    [Silver II] Title: 연속합, Time: 112 ms, Memory: 121956 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,16 +1,18 @@
 import sys
 
-input = sys.stdin.readline
 
+# 연속합.
+# n개의 정수. 연속된 몇개의 수를 선택해서 가장 큰 합 만들기
+# n 10^5 -> N^2 안됨
+
+# dp[i] : i번쨰 원소를 포함하는 최대 연속합.
 n = int(input())
 data = list(map(int, input().split()))
-dp = [-1001] * n
-dp[0] = data[0]
+dp = [-int(1e9)] * 100000
 
+dp[0] = data[0] # 당연 : 첫 시작은 무조건
 for i in range(1,n):
     dp[i] = max(dp[i-1] + data[i], data[i])
 
-#dp에 이제 연속된 수 중 최댓값이 저장되어 있을 것.
-#이 중에 최댓값을 구하면 됨.
-print(max(dp))
-    
+res = max(dp)
+print(res)
