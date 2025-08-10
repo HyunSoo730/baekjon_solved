db89d553f3e52a02e897a2d99a0a5161a94046b7
조현수 <102610889+HyunSoo730@users.noreply.github.com>
2025-08-10 16:02:32 +0900

    [Gold V] Title: 평범한 배낭, Time: 144 ms, Memory: 110324 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -9,18 +9,13 @@ for _ in range(n):
     w,v = map(int, input().split())
     data.append((w,v))
 
-dp = [[0] * (k+1) for _ in range(n+1)]
-# dp[i][w] : i번째 물건까지 고려, 무게 w이하일 때 최대 가치
+# 1차원 DP로 해결
+dp = [0] * (k+1)
+# dp[w] : 무게 w 이하일 때 최대 가치
 
-for i in range(1, n+1):
-    weight, value = data[i-1]
-    for w in range(k+1):
-        # 현재 i번째 물건을 안 넣는 경우
-        dp[i][w] = dp[i-1][w]
-        # i번째 물건을 넣을 수 있고, 넣는 게 유리한 경우
-        if w >= weight: # 넣을 수 있다면 더 좋은지 확인(이 조건이 있어야함 : 없으면 음수 인덱스 발생) -> 더 좋은쪽 선택 : max(현재값, 넣는경우값)
-            # dp[i-1][w-weight] + value의 의미 : dp[i-1][w-weight] : 현재 물건의 무게만큼 여유를 남겨두고, 이전 물건들로 얻을 수 있는 최대가치
-            # +value : 그 여유 공간에 현재 물건을 넣었을 때의 추가 가치
-            dp[i][w] = max(dp[i][w], dp[i-1][w-weight] + value)
+for weight, value in data: # 하나씩 확인
+    # 뒤에서부터 업데이트 (중복 방지)
+    for w in range(k, weight-1, -1):
+        dp[w] = max(dp[w], dp[w-weight] + value)
 
-print(dp[n][k])
+print(dp[k])
