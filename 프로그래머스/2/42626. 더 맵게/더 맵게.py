1c239d746b1adae2d7846ce32d43186cc6e6fa79
조현수 <102610889+HyunSoo730@users.noreply.github.com>
2025-10-05 15:11:51 +0900

    [level 2] Title: 더 맵게, Time: 1855.01 ms, Memory: 49 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -5,33 +5,23 @@ def solution(scoville, K):
     
     # 모든 음식의 스코빌 지수를 K이상으로 만들기 위해 섞어야 하는 최소 횟수
     
-    heap = []
-    for val in scoville:
-        heapq.heappush(heap, val)
-    res = 0
-    def count_value(data):
-        count = 0
-        for val in data:
-            if val < K:
-                return False
-        else:
-            return True
+    heapq.heapify(scoville) # O(N) : 한번에 힙 생성
+    heap = scoville
     
-    # step1. 가장 가까운 2개 꺼내기
-    flag = False
-    while not count_value(heap):
-        if len(heap) < 2:
-            flag = True
-            break
+    res = 0
+    while heap[0] < K:
+        if len(heap) < 2: # 더이상 안돼
+            return -1
+        
+        #  두개 꺼내서 합치기
         numA = heapq.heappop(heap)
         numB = heapq.heappop(heap)
-        num = numA + numB * 2
-        heapq.heappush(heap, num)
+        heapq.heappush(heap, numA + numB * 2)
         res += 1
     
-    if flag:
-        res = -1
+    print(res)
     return res
+    
     
     
     
