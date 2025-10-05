e7bee1ea7396c4f2d3f3e7c1deebdc24612a1896
조현수 <102610889+HyunSoo730@users.noreply.github.com>
2025-10-05 17:43:05 +0900

    [level 3] Title: 야근 지수, Time: 480.97 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -7,21 +7,25 @@ def solution(n, works):
     # 남은 N시간, 각 작업량 works에 대해 야근 피로도 최소화 리턴
     # 결국 가장 작업 많이 남은 애들 줄여야함 -> 그리디적인 생각
     
-    heap = []
+    heap = [-x for x in works]
+    heapq.heapify(heap)
+    """
     for x in works:
         heapq.heappush(heap, -x) # 최대힙으로 전환
-    
+    """
     for _ in range(n): # n번 반복
         if heap[0] == 0: # 더이상 X
             return 0
         work = -heapq.heappop(heap)
-        work -= 1 # 작업량 -1
-        heapq.heappush(heap, -work)
+        heapq.heappush(heap, -(work -1))
     
     res = 0
     print(heap)
+    """
     while heap:
         work = -heapq.heappop(heap)
         res += work * work
+    """
+    res = sum(x * x for x in heap)
     print(res)
     return res
