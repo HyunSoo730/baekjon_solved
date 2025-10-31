eb830ed2dc6c05eece7dbc823893f8c49750e154
조현수 <102610889+HyunSoo730@users.noreply.github.com>
2025-11-01 01:50:41 +0900

    [Gold III] Title: 과제, Time: 108 ms, Memory: 110696 KB -BaekjoonHub

--- 백준/Gold/13904. 과제/과제.py
+++ 백준/Gold/13904. 과제/과제.py
@@ -1,31 +1,30 @@
 import sys
 import heapq
 
-# 과제. 과제마다 마감일 존재. 모든 과제를 끝내지 못할 수도 있음
-# 과제마자 마감일 | 점수가 있음 -> 가장 점수를 많이 받을 수 있도록.
+# 하루에 과제 하나.
+# 과제마다 끝낼 때 얻을 수 있는 점수. 마감일이 지난 과제는 점수 X
+# 가장 많은 점수
 
 n = int(input())
 data = []
 for _ in range(n):
-    d, w = map(int, input().split())
-    data.append((d,w))
+    d,w = map(int, input().split())
+    data.append((d,w)) # 과제 마감일, 과제 점수
 
-data.sort(key = lambda x : -x[0]) # 데드라인 기준 내림차순
-
+data.sort(key = lambda x : (-x[0], -x[1])) # 과제 마감일 내림차순
+max_deadline = data[0][0]
 res = 0
 idx = 0
-heap = []
-max_deadline = data[0][0]
+heap = [] # 뽑을 수 있는 후보군. 즉 해당 날짜에 선택 가능한 애들 모음
 
-for deadline in range(max_deadline, 0, -1):
-    # step1. 데드라인 이상인, 즉 가능한 경우를 모두 추출
+for deadline in range(max_deadline, 0, -1): # 하루씩 줄여나가면서...
+    # step1. 데드라인 이상. 즉 추출 가능
     while idx < n and data[idx][0] >= deadline:
-        heapq.heappush(heap, -data[idx][1]) # 얻는 점수를 내림차순으로 넣어둬야함
+        heapq.heappush(heap, -data[idx][1]) # 점수 내림차순으로
         idx += 1
-    # step 2.  해당 날짜에 뽑아낼 수 있는 것이 있다면 최대 치 뽑아냄
+    # step2. 해당 날짜에 뽑아낼 수 있어 ? -> 최대치
     if heap:
         cost = -heapq.heappop(heap)
         res += cost
 
 print(res)
-
